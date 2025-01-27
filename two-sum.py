c4113344883f78ab09f963d881343b419fbf487c
Jin Yang <chenjinyang4192@gmail.com>
2025-01-26 21:18:54 -0500

    Another day another leetcode submission

---
+++ two-sum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        track = {}
+        for i in range(len(nums)):
+            if nums[i] in track:
+                return [i, track[nums[i]]]
+            track[target-nums[i]] = i
