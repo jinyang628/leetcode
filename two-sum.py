7b44b0f721bb9cd4065d12b6cd3c4761f7836fc4
Jin Yang <chenjinyang4192@gmail.com>
2025-01-26 21:16:30 -0500

    Delete two-sum.py

--- two-sum.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        track = {}
-        for i in range(len(nums)):
-            if nums[i] in track:
-                return [i, track[nums[i]]]
-track[target-nums[i]] = i
