d2d69a5ad620bb05e9a06e07f5b4773c16f7e9e8
Jin Yang <chenjinyang4192@gmail.com>
2025-01-30 12:55:42 -0500

    Another day another leetcode submission

--- reorganize-string.py
+++ reorganize-string.py
@@ -4,24 +4,24 @@ class Solution:
     def reorganizeString(self, s: str) -> str:
         track = Counter(s)
         heap = []
-        for item, freq in track.items():
-            heapq.heappush(heap, (-freq, item))
+        for key, freq in track.items():
+            heapq.heappush(heap, (-freq, key))
         res = ""
         while heap:
-            first_freq, first_item = heapq.heappop(heap)
-            if res and first_item == res[-1]:
+            first_negative_freq, first_key = heapq.heappop(heap)
+            if res and res[-1] == first_key:
                 if not heap:
                     return ""
-                second_freq, second_item = heapq.heappop(heap)
-                res += second_item
-                res += first_item
-                print(res)
-                if second_freq + 1 != 0:
-                    heapq.heappush(heap, (second_freq + 1, second_item))
-                if first_freq + 1 != 0:
-                    heapq.heappush(heap, (first_freq + 1, first_item))
+                second_negative_freq, second_key = heapq.heappop(heap)
+                second_negative_freq += 1
+                res += second_key
+                if second_negative_freq != 0:
+                    heapq.heappush(heap, (second_negative_freq, second_key))
+                heapq.heappush(heap, (first_negative_freq, first_key))
             else:
-                res += first_item
-                if first_freq + 1 != 0:
-                    heapq.heappush(heap, (first_freq + 1, first_item))
+                res += first_key
+                first_negative_freq += 1
+                if first_negative_freq == 0:
+                    continue
+                heapq.heappush(heap, (first_negative_freq, first_key))
         return res
