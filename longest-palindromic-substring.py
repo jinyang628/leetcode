d87b70b5c0b09887cabe29ef12f7c1b93e2f8ace
Jin Yang <chenjinyang4192@gmail.com>
2025-02-20 12:55:29 -0500

    Another day another leetcode submission

--- longest-palindromic-substring.py
+++ longest-palindromic-substring.py
@@ -1,20 +1,18 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         res = s[0]
-        # split in middle
-        def get_longest(i: int, j: int) -> str:
-            if i < 0 or j >= len(s):
-                return s[i + 1: j]
-            if s[i] != s[j]:
-                if i + 1 == j:
-                    return s[i]
-                return s[i + 1: j]
-            return get_longest(i - 1, j + 1)
+        def longestPalindrome(left: int, right: int) -> int:
+            if left < 0 or right >= len(s):
+                return s[left + 1: right]
+            if s[left] != s[right]:
+                return s[left + 1: right]
+            return longestPalindrome(left - 1, right + 1)
         for i in range(len(s) - 1):
-            odd_palindrome = get_longest(i, i)
-            even_palindrome = get_longest(i, i + 1)
-            if len(odd_palindrome) > len(res):
-                res = odd_palindrome
-            if len(even_palindrome) > len(res):
-                res = even_palindrome
+            first = longestPalindrome(i, i)
+            second = longestPalindrome(i, i + 1)
+            print(first, second)
+            if len(first) > len(res):
+                res = first
+            if len(second) > len(res):
+                res = second
         return res
