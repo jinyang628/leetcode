d93444d932f8b332b2022a070ba878518101d2f1
Jin Yang <chenjinyang4192@gmail.com>
2025-03-01 08:57:19 -0500

    Another day another leetcode submission

--- permutation-in-string.py
+++ permutation-in-string.py
@@ -1,19 +1,19 @@
 from collections import Counter
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
         left = 0
         right = len(s1)
-        s1_counter = Counter(list(s1))
-        print(s1_counter)
-        def isPermutation(left: int, right: int) -> bool:
-            s2_counter = Counter(list(s2[left: right]))
-            for key, value in s1_counter.items():
-                if s2_counter[key] != value:
-                    return False
-            return True
-        while right <= len(s2):
-            if isPermutation(left, right):
+        s1_counter = Counter(s1)
+        s2_counter = Counter(s2[:right])
+        while right < len(s2):
+            if s1_counter == s2_counter:
                 return True
+            s2_counter[s2[left]] -= 1
+            if not s2_counter[s2[left]]:
+                del s2_counter[s2[left]]
             left += 1
+            s2_counter[s2[right]] += 1
             right += 1
-        return False
+        return s1_counter == s2_counter
