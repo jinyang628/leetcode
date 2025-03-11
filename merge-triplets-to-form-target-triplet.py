c6775d6328a509683a515e77a513b211b7335cee
Jin Yang <chenjinyang4192@gmail.com>
2025-03-10 21:07:13 -0400

    Another day another leetcode submission

--- merge-triplets-to-form-target-triplet.py
+++ merge-triplets-to-form-target-triplet.py
@@ -1,11 +1,11 @@
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
-        a = b = c = False
-        for triplet in triplets:
-            if triplet[0] == target[0] and triplet[1] <= target[1] and triplet[2] <= target[2]:
-                a = True
-            if triplet[0] <= target[0] and triplet[1] == target[1] and triplet[2] <= target[2]:
-                b = True
-            if triplet[0] <= target[0] and triplet[1] <= target[1] and triplet[2] == target[2]:
-                c = True
-        return a and b and c
+        res = [False, False, False]
+        for one, two, three in triplets:
+            if one == target[0] and two <= target[1] and three <= target[2]:
+                res[0] = True 
+            if one <= target[0] and two == target[1] and three <= target[2]:
+                res[1] = True
+            if one <= target[0] and two <= target[1] and three == target[2]:
+                res[2] = True
+        return res[0] and res[1] and res[2]
