3b365c50c2d025fe1e92728eb08fdc4e1a3edf9e
Jin Yang <chenjinyang4192@gmail.com>
2025-03-12 11:21:12 -0400

    Another day another leetcode submission

--- design-add-and-search-words-data-structure.py
+++ design-add-and-search-words-data-structure.py
@@ -1,26 +1,29 @@
 class WordDictionary:
-    def __init__(self, val: Optional[str] = None, is_end: bool = False):
-        self.val = val
+    is_end: bool
+    children: dict[str, "WordDictionary"]
+    def __init__(self):
+        self.is_end = False
         self.children = {}
-        self.is_end = is_end
     def addWord(self, word: str) -> None:
         if not word:
             self.is_end = True
             return
-        if word[0] not in self.children:
-            self.children[word[0]] = WordDictionary(val=word[0])
+        if word[0] in self.children:
+            return self.children[word[0]].addWord(word[1:])
+        self.children[word[0]] = WordDictionary()
         self.children[word[0]].addWord(word[1:])
     def search(self, word: str) -> bool:
         if not word:
             return self.is_end
-        if word[0] not in self.children:
-            if word[0] != ".":    
-                return False
+        if word[0] != ".":
+            if word[0] in self.children:
+                return self.children[word[0]].search(word[1:])
+        else:
             res = False
-            for _, child in self.children.items():
-                res = res or child.search(word[1:])
+            for k, v in self.children.items():
+                res = res or v.search(word[1:])
             return res
-        return self.children[word[0]].search(word[1:])
+        return False
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
 # obj.addWord(word)
