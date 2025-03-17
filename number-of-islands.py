96d299176b9493cd214efb83ceb949be5a7b0711
Jin Yang <chenjinyang4192@gmail.com>
2025-03-17 17:32:55 -0400

    Another day another leetcode submission

--- number-of-islands.py
+++ number-of-islands.py
@@ -1,20 +1,20 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         row, col = len(grid), len(grid[0])
-        res = 0
         def dfs(r: int, c: int) -> bool:
             if r < 0 or r >= row or c < 0 or c >= col:
                 return False
-            if grid[r][c] == '0':
+            if grid[r][c] == "0":
                 return False
-            grid[r][c] = '0' 
+            grid[r][c] = "0"
             dfs(r - 1, c)
             dfs(r + 1, c)
             dfs(r, c - 1)
             dfs(r, c + 1)
             return True
+        count = 0
         for i in range(row):
             for j in range(col):
                 if dfs(i, j):
-                    res += 1
-        return res
+                    count += 1
+        return count
