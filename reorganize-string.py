78f107f19346efc6382d1d8452545ac173df7dc9
Jin Yang <chenjinyang4192@gmail.com>
2025-03-18 12:13:38 -0400

    Another day another leetcode submission

--- reorganize-string.py
+++ reorganize-string.py
@@ -1,27 +1,19 @@
 from collections import Counter
-import heapq
 class Solution:
     def reorganizeString(self, s: str) -> str:
         track = Counter(s)
+        cooldown = None
         heap = []
         for key, freq in track.items():
             heapq.heappush(heap, (-freq, key))
+        time = 0
         res = ""
         while heap:
-            first_negative_freq, first_key = heapq.heappop(heap)
-            if res and res[-1] == first_key:
-                if not heap:
-                    return ""
-                second_negative_freq, second_key = heapq.heappop(heap)
-                second_negative_freq += 1
-                res += second_key
-                if second_negative_freq != 0:
-                    heapq.heappush(heap, (second_negative_freq, second_key))
-                heapq.heappush(heap, (first_negative_freq, first_key))
-            else:
-                res += first_key
-                first_negative_freq += 1
-                if first_negative_freq == 0:
-                    continue
-                heapq.heappush(heap, (first_negative_freq, first_key))
-        return res
+            _, key = heapq.heappop(heap)
+            track[key] -= 1
+            res += key
+            if cooldown is not None and track[cooldown]:
+                heapq.heappush(heap, (-track[cooldown], cooldown))
+            cooldown = key
+            time += 1
+        return res if not track[cooldown] else ""
