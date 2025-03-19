6cdf84385074afa7cc5d770529f062d45bce74d4
Jin Yang <chenjinyang4192@gmail.com>
2025-03-19 11:24:45 -0400

    Another day another leetcode submission

--- the-kth-factor-of-n.py
+++ the-kth-factor-of-n.py
@@ -1,9 +1,17 @@
 class Solution:
     def kthFactor(self, n: int, k: int) -> int:
         res = []
-        for i in range(1, n+1):
-            if n % i == 0:
-                res.append(i)
-            if len(res) == k:
-                return res[-1]
+        for i in range(1, int(n**0.5) + 1):
+            if n % i != 0:
+                continue
+            res.append(i)
+            k -= 1
+            if k == 0:
+                return i
+        for num in res.copy()[::-1]:
+            if num ** 2 == n:
+                continue 
+            k -= 1
+            if k == 0:
+                return n // num
         return -1
