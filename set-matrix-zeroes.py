f35fc5035feba2e3234561232b9e44cb0efbfa7b
Jin Yang <chenjinyang4192@gmail.com>
2025-03-31 15:10:26 -0400

    Another day another leetcode submission

--- set-matrix-zeroes.py
+++ set-matrix-zeroes.py
@@ -2,15 +2,16 @@ class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         """
-        firstColumnZero = False
+        top_left_row_is_zero = False
         row, col = len(matrix), len(matrix[0])
+        for i in range(col):
+            if matrix[0][i] == 0:
+                top_left_row_is_zero = True
+                break
         for i in range(row):
             if matrix[i][0] == 0:
-                firstColumnZero = True
-                break
-        for j in range(col):
-            if matrix[0][j] == 0:
                 matrix[0][0] = 0
+                break
         for i in range(1, row):
             for j in range(1, col):
                 if matrix[i][j] == 0:
@@ -21,8 +22,8 @@ class Solution:
                 if matrix[i][0] == 0 or matrix[0][j] == 0:
                     matrix[i][j] = 0
         if matrix[0][0] == 0:
-            for i in range(col):
-                matrix[0][i] = 0
-        if firstColumnZero:
             for i in range(row):
                 matrix[i][0] = 0
+        if top_left_row_is_zero:
+            for i in range(col):
+                matrix[0][i] = 0
