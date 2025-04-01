c7d202ae925d4b587461e4f2c777ef66b876d1b5
Jin Yang <chenjinyang4192@gmail.com>
2025-04-01 13:30:21 -0400

    Another day another leetcode submission

--- subsets.py
+++ subsets.py
@@ -1,13 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
         res = []
-        def backtrack(subsetSoFar: list[int], idx: int):
+        def backtrack(lst: list[int], idx: int):
             if idx == len(nums):
-                res.append(subsetSoFar[:])
+                res.append(lst[:])
                 return
-            subsetSoFar.append(nums[idx])
-            backtrack(subsetSoFar, idx + 1)
-            subsetSoFar.pop()
-            backtrack(subsetSoFar, idx + 1)
+            lst.append(nums[idx])
+            backtrack(lst, idx + 1)
+            lst.pop()
+            while idx + 1 < len(nums) and nums[idx] == nums[idx + 1]:
+                idx += 1
+            backtrack(lst, idx + 1)
         backtrack([], 0)
         return res
