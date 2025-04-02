2b96c13519f225255fa85abf5df9fb13a29df8af
Jin Yang <chenjinyang4192@gmail.com>
2025-04-02 15:57:12 -0400

    Another day another leetcode submission

--- rotting-oranges.py
+++ rotting-oranges.py
@@ -1,29 +1,37 @@
 from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        row, col = len(grid), len(grid[0])
-        totalOranges = currRotten = 0
+        total = time = count = 0
         queue = deque([])
+        row, col = len(grid), len(grid[0])
         for i in range(row):
             for j in range(col):
                 if grid[i][j] != 0:
-                    totalOranges += 1
-                    if grid[i][j] == 2:
-                        queue.append((i, j, 0))
-        if not totalOranges:
-            return 0
-        while queue:
-            r, c, time = queue.popleft()
-            if r < 0 or r >= row or c < 0 or c >= col:
-                continue
-            if grid[r][c] == 0:
-                continue
-            currRotten += 1
-            if currRotten == totalOranges:
-                return time
-            grid[r][c] = 0
-            queue.append((r - 1, c, time + 1))
-            queue.append((r + 1, c, time + 1))
-            queue.append((r, c - 1, time + 1))
-            queue.append((r, c + 1, time + 1))
-        return -1
+                    total += 1
+                if grid[i][j] == 2:
+                    queue.append((i, j))
+        exit = False
+        while not exit:
+            tmp = deque([])
+            while queue:
+                r, c = queue.popleft()
+                count += 1
+                grid[r][c] = -1
+                if r > 0 and grid[r - 1][c] == 1:
+                    grid[r-1][c] = 2
+                    tmp.append((r - 1, c))
+                if r < row - 1 and grid[r + 1][c] == 1:
+                    grid[r+1][c] = 2
+                    tmp.append((r + 1, c))
+                if c > 0 and grid[r][c - 1] == 1:
+                    grid[r][c-1] = 2
+                    tmp.append((r, c - 1))
+                if c < col - 1 and grid[r][c + 1] == 1:
+                    grid[r][c+1] = 2
+                    tmp.append((r, c + 1))
+            if not tmp:
+                exit = True
+            else:
+                time += 1
+                queue = tmp
+        return time if total == count else -1
