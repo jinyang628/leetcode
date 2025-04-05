6ded0ae61737d2a04649b6057f67a33a3a063cd2
Jin Yang <chenjinyang4192@gmail.com>
2025-04-04 21:05:12 -0400

    Another day another leetcode submission

--- maximum-product-subarray.py
+++ maximum-product-subarray.py
@@ -1,14 +1,11 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
+        currMax = nums[0]
+        currMin = nums[0]
         res = nums[0]
-        negativeProductSoFar = nums[0]
-        positiveProductSoFar = nums[0]
-        for num in nums[1:]:
-            if num < 0:
-                negativeProductSoFar, positiveProductSoFar = positiveProductSoFar, negativeProductSoFar
-            negativeProductSoFar = min(num, negativeProductSoFar * num)
-            positiveProductSoFar = max(num, positiveProductSoFar * num)
-            res = max(res, positiveProductSoFar)
+        for i in range(1, len(nums)):
+            tmpMax = currMax
+            currMax = max(nums[i], tmpMax * nums[i], currMin * nums[i])
+            currMin = min(nums[i], currMin * nums[i], tmpMax * nums[i])
+            res = max(res, currMax)
         return res
