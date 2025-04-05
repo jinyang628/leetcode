01441d2cca05061ba56fc26671ec1674b75118e7
Jin Yang <chenjinyang4192@gmail.com>
2025-04-04 21:12:24 -0400

    Another day another leetcode submission

--- combination-sum.py
+++ combination-sum.py
@@ -1,16 +1,17 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        def helper(idx: int, sumSoFar: int, pathSoFar: list[int]):
-            if idx == len(candidates):
-                if sumSoFar == target:
-                    res.append(pathSoFar[:])
-                return
+        def helper(idx: int, path: list[int], sumSoFar: int) -> None:
+            if sumSoFar == target:
+                res.append(path[:])
+                return 
             if sumSoFar > target:
-                return
-            pathSoFar.append(candidates[idx])
-            helper(idx, sumSoFar + candidates[idx], pathSoFar)
-            pathSoFar.pop()
-            helper(idx + 1, sumSoFar, pathSoFar)
-        helper(0, 0, [])
+                return 
+            if idx == len(candidates):                    
+                return 
+            path.append(candidates[idx])
+            helper(idx, path, sumSoFar + candidates[idx])
+            path.pop()
+            helper(idx + 1, path, sumSoFar)
+        helper(0, [], 0)
         return res
