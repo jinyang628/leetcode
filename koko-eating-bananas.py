24439c6f3aaaed1b9ed1daa50a68e506de625362
Jin Yang <chenjinyang4192@gmail.com>
2025-04-08 19:18:01 -0400

    Another day another leetcode submission

--- koko-eating-bananas.py
+++ koko-eating-bananas.py
@@ -1,16 +1,17 @@
+import math 
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def eat(speed: int) -> int:
-            timeNeeded =0
-            for pile in piles:
-                timeNeeded += math.ceil(pile / speed)
-            return timeNeeded
+        def canFinish(speed: int) -> bool:
+            time = 0
+            for num in piles:
+                time += math.ceil(num / speed)
+            return time <= h
         left = 1
         right = max(piles)
         while left <= right:
             mid = left + (right - left) // 2
-            if eat(mid) > h:
-                left = mid + 1
-            else:
+            if canFinish(mid):
                 right = mid - 1
+            else:
+                left = mid + 1
         return left
