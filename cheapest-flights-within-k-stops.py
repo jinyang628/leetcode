c02a948a97032048fb26140c8d6e2d9ac8f977b4
Jin Yang <chenjinyang4192@gmail.com>
2025-04-09 13:16:51 -0400

    Another day another leetcode submission

--- cheapest-flights-within-k-stops.py
+++ cheapest-flights-within-k-stops.py
@@ -1,21 +1,21 @@
-import heapq
 from collections import defaultdict
+import heapq
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         track = defaultdict(list)
-        for f, t, p in flights:
-            track[f].append((p, t))
-        heap = [(0, 0, src)] # costSoFar, stops, node
-        visited = {} # key is node and value is stops
+        distances = {}
+        for i in range(n):
+            distances[i] = float('inf')
+        for s, d, dist in flights:
+            track[s].append((d, dist))
+        heap = [(0, 0, src)]
         while heap:
-            costSoFar, stops, node = heapq.heappop(heap)
-            if node == dst:
-                return costSoFar
-            if stops > k:
-                continue
-            if node in visited and visited[node] <= stops:
-                continue
-            visited[node] = stops
-            for price, target in track[node]:
-                heapq.heappush(heap, (costSoFar + price, stops + 1,  target))
-        return -1
+            steps, dist, node = heapq.heappop(heap)
+            if steps > k + 1:
+                break
+            print(steps, dist, node, distances)
+            if distances[node] > dist:
+                distances[node] = dist
+                for neighbor, weight in track[node]:
+                    heapq.heappush(heap, (steps + 1, weight + dist, neighbor))
+        return distances[dst] if distances[dst] != float('inf') else -1
