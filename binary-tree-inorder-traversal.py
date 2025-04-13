d47e0b21b31449cecf4edd92679ba33ff3fe78c7
Jin Yang <chenjinyang4192@gmail.com>
2025-04-13 18:37:53 -0400

    Another day another leetcode submission

---
+++ binary-tree-inorder-traversal.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        def helper(node: Optional[TreeNode]) -> None:
+            if not node:
+                return 
+            helper(node.left)
+            res.append(node.val)
+            helper(node.right)
+        helper(root)
+        return res
