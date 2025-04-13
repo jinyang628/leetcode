564342c3028fd3a136a583dc753de61094df505c
Jin Yang <chenjinyang4192@gmail.com>
2025-04-13 18:54:27 -0400

    Another day another leetcode submission

--- longest-common-subsequence.py
+++ longest-common-subsequence.py
@@ -1,17 +1,13 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        # i = j -> 1 + lcs(i - 1, j - 1)
-        # i != j -> max(lcs(i - 1, j), lcs(i, j - 1))
-        row = len(text1)
-        col = len(text2)
-        res = [[0] * col for _ in range(row)]
-        for i in range(row):
-            for j in range(col):
-                if text1[i] == text2[j]:
-                    if i == 0 or j == 0:
-                        res[i][j] = 1
-                    else:
-                        res[i][j] = res[i - 1][j - 1] + 1
+        # if text1[i] == text2[j], dp[i][j] = dp[i - 1][j - 1] + 1
+        # else, dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        row, col = len(text1), len(text2)
+        dp = [[0] * (col + 1) for _ in range(row + 1)]
+        for i in range(1, row + 1):
+            for j in range(1, col + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
                 else:
-                    res[i][j] = max(res[i - 1][j], res[i][j - 1])
-        return res[row - 1][col - 1]
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        return dp[-1][-1]
