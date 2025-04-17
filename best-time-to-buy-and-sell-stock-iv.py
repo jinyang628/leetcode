036034a87dbb14708fe95b31d524e866fac00282
Jin Yang <chenjinyang4192@gmail.com>
2025-04-16 21:29:01 -0400

    Another day another leetcode submission

--- best-time-to-buy-and-sell-stock-iv.py
+++ best-time-to-buy-and-sell-stock-iv.py
@@ -1,17 +1,17 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
         dp = [[[0] * (k + 1) for _ in range(2)] for _ in range(len(prices) + 1)]
-        for idx in range(len(prices) - 1, -1, -1):
-            for can_sell in range(2):
-                for i in range(1, k + 1):
-                    if can_sell:
-                        dp[idx][can_sell][i] = max(
-                            dp[idx + 1][can_sell][i], # dont sell today
-                            prices[idx] + dp[idx + 1][0][i - 1] # sell today
-                        )
+        for i in range(len(prices) - 1, -1, -1):
+            for can_buy in range(2):
+                for transactions_left in range(1, k + 1):
+                    if can_buy:
+                        dp[i][can_buy][transactions_left] = max(
+                            -prices[i] + dp[i + 1][0][transactions_left],
+                            dp[i + 1][can_buy][transactions_left]
+                        )  
                     else:
-                        dp[idx][can_sell][i] = max(
-                            dp[idx + 1][can_sell][i], # dont buy today
-                            - prices[idx] + dp[idx + 1][1][i]# buy today
+                        dp[i][can_buy][transactions_left] = max(
+                            prices[i] + dp[i + 1][1][transactions_left - 1],
+                            dp[i + 1][can_buy][transactions_left]
                         )
-        return dp[0][0][k]
+        return dp[0][1][k]
