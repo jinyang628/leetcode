51d85529c7c3a7b5973f7c70b3c978ad0f4ec18c
Jin Yang <chenjinyang4192@gmail.com>
2025-04-19 20:54:00 -0400

    Another day another leetcode submission

--- lru-cache.py
+++ lru-cache.py
@@ -1,42 +1,43 @@
 class Node:
-    def __init__(self, key: int, val: int, prev_node: Optional["Node"] = None, next_node: Optional["Node"] = None):
+    def __init__(self, key: int, val: int, prev: Optional["Node"] = None, next: Optional["Node"] = None):
         self.key = key
         self.val = val
-        self.prev_node = prev_node
-        self.next_node = next_node
+        self.prev = prev
+        self.next = next
 class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {}
+        self.track = {} # key is the key, value is a pointer to the Node
         self.oldest = Node(0, 0)
-        self.latest = Node(0, 0)
-        self.oldest.next_node = self.latest
-        self.latest.prev_node = self.oldest
+        self.youngest = Node(0, 0)        
+        self.oldest.next = self.youngest
+        self.youngest.prev = self.oldest
     def get(self, key: int) -> int:
-        if key in self.cache:
-            node = self.cache[key]
-            self.delete(node)
-            self.insert(node)
-            return self.cache[key].val
+        if key in self.track:
+            self.remove(self.track[key])
+            self.insert(self.track[key])
+            return self.track[key].val
         return -1
+    def insert(self, node: Node):
+        prev_youngest = self.youngest.prev
+        prev_youngest.next = node
+        self.youngest.prev = node
+        node.next = self.youngest 
+        node.prev = prev_youngest
+    def remove(self, node: Node) -> None:
+        prev = node.prev
+        next = node.next
+        prev.next = next
+        next.prev = prev
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.delete(self.cache[key])
-        self.insert(Node(key, value))
-        if len(self.cache) > self.capacity:
-            self.delete(self.oldest.next_node)
-    def insert(self, node: Node) -> None:
-        prev, next_node = self.latest.prev_node, self.latest
-        prev.next_node = node
-        node.prev_node = prev
-        node.next_node = next_node
-        next_node.prev_node = node 
-        self.cache[node.key] = node
-    def delete(self, node: Node) -> None:
-        prev, next_node = node.prev_node, node.next_node
-        prev.next_node = next_node
-        next_node.prev_node = prev
-        del self.cache[node.key]
+        if key in self.track:
+            self.remove(self.track[key])
+        self.track[key] = Node(key, value)
+        self.insert(self.track[key])
+        if len(self.track) > self.capacity:
+            oldest = self.oldest.next 
+            self.remove(oldest)
+            del self.track[oldest.key]
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
