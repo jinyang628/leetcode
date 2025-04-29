36f99fcca0faf66127497cdfa5a746027acd01db
Jin Yang <chenjinyang4192@gmail.com>
2025-04-29 13:10:50 -0400

    Another day another leetcode submission

--- letter-combinations-of-a-phone-number.py
+++ letter-combinations-of-a-phone-number.py
@@ -2,16 +2,23 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return []
-        track = {"2": ["a", "b", "c"], "3": ["d", "e", "f"], "4": ["g", "h", "i"], "5": ["j", "k", "l"], "6": ["m", "n", "o"], "7": ["p", "q", "r", "s"], "8": ["t", "u", "v"], "9": ["w", "x", "y", "z"]}
         res = []
-        def backtrack(idx: int, path: list[str]):
+        track = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"]
+        }
+        def backtrack(idx: int, path: list[int]) -> None:
             if idx == len(digits):
                 res.append("".join(path))
                 return
-            currDigit = digits[idx]
-            for char in track[currDigit]:
-                path.append(char)
-                backtrack(idx + 1, path)
-                path.pop()
+            curr = digits[idx]
+            for char in track[curr]:
+                backtrack(idx + 1, path + [char])
         backtrack(0, [])
         return res
