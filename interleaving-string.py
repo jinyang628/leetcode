5c9ddfe9b4317f3af971f235a932b0a066628915
Jin Yang <chenjinyang4192@gmail.com>
2025-05-03 04:29:30 -0400

    Another day another leetcode submission

--- interleaving-string.py
+++ interleaving-string.py
@@ -1,17 +1,18 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        row, col = len(s1), len(s2)
-        if row + col != len(s3):
+        if len(s1) + len(s2) != len(s3):
             return False
-        dp = [[False] * (col + 1) for _ in range(row + 1)]
-        dp[0][0] = True
-        for i in range(1, row + 1):
-            dp[i][0] = dp[i - 1][0] and s1[i - 1] == s3[i - 1]
-        for j in range(1, col + 1):
-            dp[0][j] = dp[0][j - 1] and s2[j - 1] == s3[j - 1]
-        for i in range(1, row + 1):
-            for j in range(1, col + 1):
-                match_1 = dp[i - 1][j] and s3[i + j - 1] == s1[i - 1]
-                match_2 = dp[i][j - 1] and s3[i + j - 1] == s2[j - 1]
-                dp[i][j] = match_1 or match_2
-        return dp[row][col]
+        memo = {}
+        def helper(idx1: int, idx2: int, idx3: int) -> bool:
+            if idx3 == len(s3):
+                return True
+            if (idx1, idx2) in memo:
+                return memo[(idx1, idx2)]
+            res = False
+            if idx1 < len(s1) and s1[idx1] == s3[idx3]:
+                res = helper(idx1 + 1, idx2, idx3 + 1)
+            if idx2 < len(s2) and s2[idx2] == s3[idx3]:
+                res = res or helper(idx1, idx2 + 1, idx3 + 1)
+            memo[(idx1, idx2)] = res
+            return res
+        return helper(0,0,0)
