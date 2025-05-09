4ae1f2b2909678f4eb9d114929b15f8fbdbba5ea
Jin Yang <chenjinyang4192@gmail.com>
2025-05-09 09:57:30 -0400

    Another day another leetcode submission

--- count-elements-with-strictly-smaller-and-greater-elements.py
+++ count-elements-with-strictly-smaller-and-greater-elements.py
@@ -1,15 +1,5 @@
 class Solution:
     def countElements(self, nums: List[int]) -> int:
-        track = {}
-        res = 0
-        new_nums = sorted(nums)
-        while len(new_nums) >= 2 and new_nums[0] == new_nums[1]:
-            new_nums.pop(0)
-        while len(new_nums) >= 2 and new_nums[-1] == new_nums[-2]:
-            new_nums.pop()
-        for i in range(len(new_nums)):
-            track[new_nums[i]] = i
-        for num in nums:
-            if 0 < track[num] < len(new_nums) - 1:
-                res += 1
-        return res
+        smallest = min(nums)
+        largest = max(nums)
+        return sum(1 for i in nums if smallest < i < largest)
