bbca22aec03dab225f57211e20f54f415c729c8f
Jin Yang <chenjinyang4192@gmail.com>
2025-05-15 21:14:11 +0800

    Another day another leetcode submission

--- reorder-list.py
+++ reorder-list.py
@@ -7,30 +7,21 @@ class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         """
-        if not head or not head.next:
-            return 
         slow = fast = head
-        prev = None
         while fast and fast.next:
-            fast = fast.next.next
-            prev = slow
             slow = slow.next
-        prev.next = None
-        second = None
-        while slow:
-            next_node = slow.next
-            slow.next = second
-            second = slow
-            slow = next_node
-        first = head
-        while first and second:
-            next_first = first.next
-            next_second = second.next
+            fast = fast.next.next
+        tail = slow.next
+        slow.next = None
+        prev = None
+        while tail:
+            next_node = tail.next
+            tail.next = prev
+            prev = tail
+            tail = next_node
+        first, second = head, prev
+        while second:
+            next_head, next_tail = first.next, second.next
             first.next = second
-            second.next = next_first
-            first = next_first
-            second = next_second
-        copy = head
-        while copy.next:
-            copy = copy.next
-        copy.next = second
+            second.next = next_head
+            first, second = next_head, next_tail
