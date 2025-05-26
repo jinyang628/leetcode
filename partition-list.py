1037774efd575a23f2bdad2067becb69e357a8ae
Jin Yang <chenjinyang4192@gmail.com>
2025-05-26 14:55:33 +0800

    Another day another leetcode submission

--- partition-list.py
+++ partition-list.py
@@ -5,18 +5,19 @@
 #         self.next = next
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-        tailDummy = ListNode(0)
-        headDummy = ListNode(0)
-        copyHead = headDummy
-        copyTail = tailDummy
-        while head:
-            if head.val < x:
-                copyHead.next = head
-                copyHead = copyHead.next
+        left = ListNode(0)
+        right = ListNode(0)
+        copy = head
+        copy_left = left
+        copy_right = right
+        while copy:
+            if copy.val < x:
+                copy_left.next = copy
+                copy_left = copy_left.next
             else:
-                copyTail.next = head
-                copyTail = copyTail.next
-            head = head.next
-        copyTail.next = None
-        copyHead.next = tailDummy.next
-        return headDummy.next
+                copy_right.next = copy
+                copy_right = copy_right.next
+            copy = copy.next
+        copy_right.next = None
+        copy_left.next = right.next
+        return left.next
