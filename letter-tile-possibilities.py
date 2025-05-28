cbc3646eb8ad04e04f5c3ef799463b60a22798e3
Jin Yang <chenjinyang4192@gmail.com>
2025-05-28 13:46:14 +0800

    Another day another leetcode submission

--- letter-tile-possibilities.py
+++ letter-tile-possibilities.py
@@ -1,30 +1,15 @@
+from collections import defaultdict, Counter
 class Solution:
-    def numTilePossibilities(self, tiles: str) -> int:
-        factorial_results = {1: 1}
-        seen_sub_groups = set()
-        tiles = sorted(tiles)
-        ans = 0
-        def factorial(n):
-            if n in factorial_results: return factorial_results[n]
-            factorial_results[n] = n * factorial(n - 1)
-            return factorial_results[n]
-        def counter(arr):
-            # I like to make my own counter just to show I can
-            temp = {}
-            for x in arr:
-                temp[x] = temp.get(x, 0) + 1
-            return temp 
-        def arrangements_with_duplicates(letter_counts):
-            k = sum(letter_counts.values())
-            duplicates = [x for x in letter_counts.values() if x > 1]
-            temp = factorial(k)
-            for x in duplicates:
-                temp //= factorial(x)
-            return temp
-        for i in range(1, len(tiles) + 1):
-            for comb in itertools.combinations(tiles, i):
-                if comb in seen_sub_groups:
+     def numTilePossibilities(self, tiles: str) -> int:
+        def backtrack(count: dict):
+            total = 0
+            for char in count:
+                if count[char] == 0:
                     continue
-                ans += arrangements_with_duplicates(counter(comb))
-                seen_sub_groups.add(comb)
-        return ans
+                total += 1
+                count[char] -= 1
+                total += backtrack(count)
+                count[char] += 1
+            return total
+        count = Counter(tiles)
+        return backtrack(count)
