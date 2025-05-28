35bcf5a14e236dc1293934055237efe421e7ab0d
Jin Yang <chenjinyang4192@gmail.com>
2025-05-28 13:17:35 +0800

    Another day another leetcode submission

--- car-pooling.py
+++ car-pooling.py
@@ -1,22 +1,19 @@
 from collections import defaultdict
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        depart_track = defaultdict(int) # key is stop, value is number of ppl
-        arrival_track = defaultdict(int) # same as above
-        for p, d, a in trips:
-            depart_track[d] += p
-            arrival_track[a] += p
-        first_stop = min(depart_track)
-        last_stop = max(arrival_track)
-        ppl = 0
-        print(depart_track)
-        print(arrival_track)
-        for i in range(first_stop, last_stop):
-            if i in depart_track:
-                ppl += depart_track[i]
-            if i in arrival_track:
-                ppl -= arrival_track[i]
-            print(ppl)
-            if ppl > capacity:
+        depart_track = defaultdict(int)
+        arrive_track = defaultdict(int)
+        start = float('inf')
+        end = float('-inf')
+        for num, f, t in trips:
+            depart_track[f] += num
+            arrive_track[t] += num
+            start = min(start, f)
+            end = max(end, t)
+        curr = 0
+        for i in range(start, end + 1):
+            curr += depart_track[i]
+            curr -= arrive_track[i]
+            if curr > capacity:
                 return False
         return True
