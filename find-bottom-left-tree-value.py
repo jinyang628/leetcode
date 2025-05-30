bf48bf3536793df3ad0e1279a3be160165692066
Jin Yang <chenjinyang4192@gmail.com>
2025-05-30 11:12:09 +0800

    Another day another leetcode submission

--- find-bottom-left-tree-value.py
+++ find-bottom-left-tree-value.py
@@ -6,13 +6,13 @@
 #         self.right = right
 class Solution:
     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
-        maxSoFar = (-1, None)
+        maxSoFar = [-1, None]
         def helper(node: Optional[TreeNode], level: int):
-            nonlocal maxSoFar
             if not node:
                 return 
             if level > maxSoFar[0]:
-                maxSoFar = (level, node.val)
+                maxSoFar[0] = level
+                maxSoFar[1] = node.val
             helper(node.left, level + 1)
             helper(node.right, level + 1)
         helper(root, 0)
