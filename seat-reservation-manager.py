8ffff7ff05818410d1d933da5e20ed3283064110
Jin Yang <chenjinyang4192@gmail.com>
2025-05-30 17:04:39 +0800

    Another day another leetcode submission

---
+++ seat-reservation-manager.py
@@ -0,0 +1,14 @@
+import heapq
+class SeatManager:
+    def __init__(self, n: int):
+        heap = [i for i in range(1, n + 1)]
+        heapq.heapify(heap)
+        self.heap = heap
+    def reserve(self) -> int:
+        return heapq.heappop(self.heap)
+    def unreserve(self, seatNumber: int) -> None:
+        heapq.heappush(self.heap, seatNumber)
+# Your SeatManager object will be instantiated and called as such:
+# obj = SeatManager(n)
+# param_1 = obj.reserve()
+# obj.unreserve(seatNumber)
