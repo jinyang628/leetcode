1545eb512668c492fb678ca38865ac376dc49079
Jin Yang <chenjinyang4192@gmail.com>
2025-06-04 19:05:17 +0800

    Another day another leetcode submission

--- replace-elements-with-greatest-element-on-right-side.py
+++ replace-elements-with-greatest-element-on-right-side.py
@@ -1,18 +1,9 @@
-from collections import deque
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
-        queue = deque([])
-        for num in arr:
-            if not queue or queue[-1] >= num:
-                queue.append(num)
-            else:
-                while queue and queue[-1] < num:
-                    queue.pop()
-                queue.append(num)
-        queue.append(-1)
-        res = []
-        for num in arr:
-            if num == queue[0]:
-                queue.popleft()
-            res.append(queue[0])
-        return res
+        maxSoFar = 0
+        for i in range(len(arr) - 1, -1, -1):
+            tmp = maxSoFar
+            maxSoFar = max(arr[i], maxSoFar)
+            arr[i] = tmp
+        arr[-1] = -1
+        return arr
