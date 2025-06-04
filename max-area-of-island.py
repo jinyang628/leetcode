b3c5a3d667eca8d5f0505d8d7bd954c82aca4e6a
Jin Yang <chenjinyang4192@gmail.com>
2025-06-04 18:59:48 +0800

    Another day another leetcode submission

--- max-area-of-island.py
+++ max-area-of-island.py
@@ -1,17 +1,17 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        maxSoFar = 0
         row, col = len(grid), len(grid[0])
-        def dfs(r: int, c: int) -> int:
-            if r < 0 or r >= row or c < 0 or c >= col:
+        def getIslandSize(r: int, c: int) -> int:
+            if r < 0 or c < 0 or r >= row or c >= col:
                 return 0
-            if grid[r][c] ==  0:
+            if grid[r][c] == 0 or grid[r][c] == -1:
                 return 0
-            res = 1
-            grid[r][c] = 0
-            res += dfs(r - 1, c) + dfs(r + 1, c) + dfs(r, c - 1) + dfs(r, c + 1)
-            return res
+            grid[r][c] = -1
+            return 1 + getIslandSize(r - 1, c) + getIslandSize(r + 1, c) + getIslandSize(r, c - 1) + getIslandSize(r, c + 1) 
+        res = 0
         for i in range(row):
             for j in range(col):
-                maxSoFar = max(maxSoFar, dfs(i, j))
-        return maxSoFar
+                if grid[i][j] == -1:
+                    continue
+                res = max(res, getIslandSize(i, j))
+        return res
