823c0e77bb93123048a97947fbc9f0985ea9f8ba
Jin Yang <chenjinyang4192@gmail.com>
2025-06-05 10:46:28 +0800

    Another day another leetcode submission

--- climbing-stairs.py
+++ climbing-stairs.py
@@ -2,9 +2,9 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n == 1:
             return 1
-        res = [0] * (n + 1)
-        res[0] = 1
-        res[1] = 1
-        for i in range(2, len(res)):
-            res[i] = res[i - 2] + res[i - 1]
-        return res[-1]
+        dp = [0] * n
+        dp[0] = 1
+        dp[1] = 2
+        for i in range(2, n):
+            dp[i] = dp[i - 2] + dp[i - 1]
+        return dp[-1]
