7a540787346e9fc543f69825151335812dbdb5d7
Jin Yang <chenjinyang4192@gmail.com>
2025-06-08 11:43:56 +0800

    Another day another leetcode submission

--- jump-game.py
+++ jump-game.py
@@ -1,7 +1,9 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        lastPossibleIdx = len(nums) - 1
+        if len(nums) <= 1:
+            return True
+        lastIdxReached = len(nums) - 1
         for i in range(len(nums) - 2, -1, -1):
-            if nums[i] + i >= lastPossibleIdx:
-                lastPossibleIdx = i
-        return lastPossibleIdx == 0
+            if nums[i] + i >= lastIdxReached:
+                lastIdxReached = i
+        return lastIdxReached == 0
