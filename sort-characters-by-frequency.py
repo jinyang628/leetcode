e8987b0aeafa3b8f2e1d8bc18a5a4cfd1383d5de
Jin Yang <chenjinyang4192@gmail.com>
2025-06-08 12:27:21 +0800

    Another day another leetcode submission

--- sort-characters-by-frequency.py
+++ sort-characters-by-frequency.py
@@ -1,13 +1,10 @@
-from collections import Counter
-import heapq
 class Solution:
     def frequencySort(self, s: str) -> str:
-        count = Counter(s)
-        heap = []
-        for key, freq in count.items():
-            heapq.heappush(heap, (-freq, key))
-        res = []
-        while heap:
-            negative_freq, key = heapq.heappop(heap)
-            res.extend([key] * -negative_freq)
-        return "".join(res)
+        counter = Counter(s)
+        pq = [(-freq, char) for char, freq in counter.items()]
+        heapq.heapify(pq)
+        result = ''
+        while pq:
+            freq, char = heapq.heappop(pq)
+            result += char * -freq
+        return result
