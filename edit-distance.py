1241749936e146c5b9d5e04d7c1d164971c05235
Jin Yang <chenjinyang4192@gmail.com>
2025-06-09 15:56:57 +0800

    Another day another leetcode submission

--- edit-distance.py
+++ edit-distance.py
@@ -1,21 +1,19 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        """
-        """
-        row, col = len(word1), len(word2)
-        dp = [[0] * (col + 1) for _ in range(row + 1)]
-        for i in range(row + 1): # delete i characters
+        row, col = len(word1) + 1, len(word2) + 1
+        dp = [[0] * (col) for _ in range(row)]
+        for i in range(1, row):
             dp[i][0] = i
-        for j in range(col + 1): # insert j characters
+        for j in range(1, col):
             dp[0][j] = j
-        for i in range(1, row + 1):
-            for j in range(1, col + 1):
+        for i in range(1, row):
+            for j in range(1, col):
                 if word1[i - 1] == word2[j - 1]:
                     dp[i][j] = dp[i - 1][j - 1]
                 else:
                     dp[i][j] = 1 + min(
-                        dp[i][j - 1],
+                        dp[i - 1][j - 1],
                         dp[i - 1][j],
-                        dp[i - 1][j - 1]
+                        dp[i][j - 1]
                     )
-        return dp[row][col]
+        return dp[-1][-1]
