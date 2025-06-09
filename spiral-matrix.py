f150589c6a49936698468a4cc33349a74b5a0cc1
Jin Yang <chenjinyang4192@gmail.com>
2025-06-09 16:34:28 +0800

    Another day another leetcode submission

--- spiral-matrix.py
+++ spiral-matrix.py
@@ -1,43 +1,34 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        left = 0
+        left = top = 0
         right = len(matrix[0]) - 1
-        top = 0
-        down = len(matrix) - 1
+        bottom = len(matrix) - 1
         res = []
-        while left <= right and top <= down:
-            copy_top = top
-            copy_down = down
-            copy_left = left
-            copy_right = right
+        while left <= right and top <= bottom:
+            original_left = left
             while left <= right:
                 res.append(matrix[top][left])
                 left += 1
-            left = copy_left
-            copy_top += 1
-            top = copy_top
-            if top > down:
-                break
-            while top <= down:
+            left = original_left
+            top += 1
+            original_top = top
+            while top <= bottom:
                 res.append(matrix[top][right])
                 top += 1
-            top = copy_top
-            copy_right -= 1
-            right = copy_right
-            if right < left:
+            top = original_top
+            right -= 1
+            original_right = right
+            if not left <= right or not top <= bottom:
                 break
             while right >= left:
-                res.append(matrix[down][right])
+                res.append(matrix[bottom][right])
                 right -= 1
-            right = copy_right
-            copy_down -= 1
-            down = copy_down
-            if down < top:
-                break
-            while down >= top:
-                res.append(matrix[down][left])
-                down -= 1
-            down = copy_down
-            copy_left += 1
-            left = copy_left
+            right = original_right
+            bottom -= 1
+            original_bottom = bottom
+            while bottom >= top:
+                res.append(matrix[bottom][left])
+                bottom -= 1
+            bottom = original_bottom
+            left += 1
         return res
