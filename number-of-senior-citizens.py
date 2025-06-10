e96747616939885c0ae6e810624edbd34ec69471
Jin Yang <chenjinyang4192@gmail.com>
2025-06-10 16:53:23 +0800

    Another day another leetcode submission

---
+++ number-of-senior-citizens.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        res = 0
+        for detail in details:
+            age = int(detail[11:13])
+            if age > 60:
+                res += 1
+        return res
