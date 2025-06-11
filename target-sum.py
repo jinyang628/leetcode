12e96b0a8add2559facc699879b9d584492d0569
Jin Yang <chenjinyang4192@gmail.com>
2025-06-11 14:05:02 +0800

    Another day another leetcode submission

--- target-sum.py
+++ target-sum.py
@@ -1,21 +1,15 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        total_sum = sum(nums)
-        if target > total_sum or target < -total_sum:
-            return 0
-        col = 2 * total_sum + 1
+        total = sum(nums)
+        col = total * 2 + 1
         row = len(nums)
-        dp = [[0] * col for _ in range(row)]
-        mid = col // 2
-        dp[0][mid + nums[0]] += 1
-        dp[0][mid - nums[0]] += 1
-        for i in range(row - 1):
-            for j in range(col):
-                if not dp[i][j]:
-                    continue
-                curr = nums[i + 1]
-                dp[i + 1][j + curr] += dp[i][j]
-                dp[i + 1][j - curr] += dp[i][j]
-        res = dp[-1][mid + target]
-        print(dp)
-        return res
+        dp = [[0] * (col + 2) for _ in range(row + 1)]
+        dp[0][total + 1] =  1
+        for i in range(1, row + 1):
+            for j in range(1, col + 1):
+                if j - nums[i - 1] >= 0:
+                    dp[i][j] += dp[i - 1][j - nums[i - 1]]
+                if j + nums[i - 1] < col + 2:
+                    dp[i][j] += dp[i - 1][j + nums[i - 1]]
+        target_idx = (total + target + 1)
+        return dp[row][target_idx] if target_idx < (col + 2) else 0
