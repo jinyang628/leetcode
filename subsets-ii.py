ac63e90c8e927416f64a990ce860fc7212e16386
Jin Yang <chenjinyang4192@gmail.com>
2025-06-15 02:43:46 +0800

    Another day another leetcode submission

--- subsets-ii.py
+++ subsets-ii.py
@@ -1,7 +1,7 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        res = []
         nums.sort()
+        res = []
         def backtrack(idx: int, path: list[int]):
             if idx == len(nums):
                 res.append(path[:])
@@ -9,8 +9,8 @@ class Solution:
             path.append(nums[idx])
             backtrack(idx + 1, path)
             path.pop()
-            curr = nums[idx]
-            while idx < len(nums) and nums[idx] == curr:
+            idx += 1
+            while idx < len(nums) and nums[idx] == nums[idx - 1]:
                 idx += 1
             backtrack(idx, path)
         backtrack(0, [])
