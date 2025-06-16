3ace2ed08406e2d6853e98ff72fd2c6970765a06
Jin Yang <chenjinyang4192@gmail.com>
2025-06-17 04:10:11 +0800

    Another day another leetcode submission

---
+++ valid-perfect-square.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        if num == 1:
+            return True
+        left = 1
+        right = num // 2
+        while left <= right:
+            mid = left + (right - left) // 2
+            square = mid ** 2
+            if square == num:
+                return True
+            elif square < num:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return False
