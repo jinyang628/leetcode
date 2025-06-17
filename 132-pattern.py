e0981bb35fc4756290148efa291d03aef6c8faf5
Jin Yang <chenjinyang4192@gmail.com>
2025-06-17 13:02:20 +0100

    Another day another leetcode submission

---
+++ 132-pattern.py
@@ -0,0 +1,12 @@
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        currMin = nums[0]
+        stack = []
+        for num in nums[1:]:
+            while stack and stack[-1][0] <= num:
+                stack.pop()
+            if stack and stack[-1][1] < num:
+                return True
+            stack.append([num, currMin])
+            currMin = min(currMin, num)
+        return False
