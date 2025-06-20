0b09d010198980b8cce032f96a81453841a0193a
Jin Yang <chenjinyang4192@gmail.com>
2025-06-20 14:51:01 +0100

    Another day another leetcode submission

--- count-days-without-meetings.py
+++ count-days-without-meetings.py
@@ -1,22 +1,12 @@
 class Solution:
     def countDays(self, days: int, meetings: List[List[int]]) -> int:
-        meetings.sort(key=lambda x: (x[1], x[0]))
-        merged = [meetings[0]]
-        for i in range(1, len(meetings)):
-            start, end = meetings[i]                
-            while merged and start <= merged[-1][1]:
-                prev_start, _ = merged.pop()
-                start = min(prev_start, start)
-            merged.append([start, end])
-        res = pointer = 0
-        i = 1
-        while i <= days:
-            start, end = merged[pointer]
-            if i < start:
-                res += (start - i)
-            i = end + 1
-            pointer += 1
-            if pointer >= len(merged):
-                res += (days - end)
-                break
-        return res
+        meetings.sort()
+        currDay = 1
+        total = 0
+        for start, end in meetings:
+            if currDay < start:
+                total += (start - currDay)
+            currDay = max(currDay, end + 1)
+        if days >= currDay:
+            total += (days - currDay) + 1
+        return total
