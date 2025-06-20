b976fd8330148117ab021170c5d485efaee56ae6
Jin Yang <chenjinyang4192@gmail.com>
2025-06-20 16:31:43 +0100

    Another day another leetcode submission

--- apply-operations-to-an-array.py
+++ apply-operations-to-an-array.py
@@ -4,18 +4,12 @@ class Solution:
             if nums[i] == nums[i + 1]:
                 nums[i] *= 2
                 nums[i + 1] = 0
-        left = right = None
-        for i in range(len(nums)):
-            if not nums[i]:
-                left = right = i
-                while right < len(nums) and nums[right] == 0:
-                    right += 1
-                break
-        if left == None:
-            return nums
-        while right < len(nums) and left < len(nums):
-            nums[left], nums[right] = nums[right], nums[left]
-            while right < len(nums) and nums[right] == 0:
-                right += 1
-            left += 1
-        return nums
+        res = []
+        count = 0
+        for num in nums:
+            if num:
+                res.append(num)
+            else:
+                count += 1
+        res.extend([0] * count)
+        return res
