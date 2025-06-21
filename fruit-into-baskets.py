25c8042e6273deaeececa2b6dd2000428127f3a9
Jin Yang <chenjinyang4192@gmail.com>
2025-06-21 07:23:10 +0100

    Another day another leetcode submission

--- fruit-into-baskets.py
+++ fruit-into-baskets.py
@@ -4,17 +4,12 @@ class Solution:
         left = right = maxSoFar = 0
         counter = Counter()
         while right < len(fruits):
-            if right < 2:
-                counter[fruits[right]] += 1
-                right += 1
-                maxSoFar = max(maxSoFar, right - left)
-                continue
             counter[fruits[right]] += 1
             while len(counter) > 2:
                 counter[fruits[left]] -= 1
                 if not counter[fruits[left]]:
                     del counter[fruits[left]]
                 left += 1
+            maxSoFar = max(maxSoFar, right - left + 1)
             right += 1
-            maxSoFar = max(maxSoFar, right - left)
         return maxSoFar
