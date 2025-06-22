c47e735de8e1f20a08f2c35cb50124912bd7daca
Jin Yang <chenjinyang4192@gmail.com>
2025-06-22 12:11:38 +0100

    Another day another leetcode submission

--- merge-in-between-linked-lists.py
+++ merge-in-between-linked-lists.py
@@ -5,15 +5,15 @@
 #         self.next = next
 class Solution:
     def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
-        copy_2 = list2
-        while copy_2.next:
-            copy_2 = copy_2.next
-        copy_1 = list1
+        head_list1 = list1
         for _ in range(a - 1):
-            copy_1 = copy_1.next
-        tail_1 = copy_1
+            list1 = list1.next
+        tail_list1 = list1
         for _ in range(b - a + 2):
-            tail_1 = tail_1.next
-        copy_1.next = list2
-        copy_2.next = tail_1
-        return list1
+            tail_list1 = tail_list1.next
+        head_list2 = list2
+        while list2.next:
+            list2 = list2.next
+        list1.next = head_list2
+        list2.next = tail_list1
+        return head_list1
