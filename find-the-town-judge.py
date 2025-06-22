79c9e8eb4cbda023c63899948a080e542d5bbb95
Jin Yang <chenjinyang4192@gmail.com>
2025-06-22 07:51:41 +0100

    Another day another leetcode submission

--- find-the-town-judge.py
+++ find-the-town-judge.py
@@ -1,12 +1,11 @@
-from collections import defaultdict
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        a_trust_b_track = defaultdict(list)
-        b_trust_a_track = defaultdict(list)
+        indegree = defaultdict(int)
+        outdegree = defaultdict(int)
         for a, b in trust:
-            a_trust_b_track[a].append(b)
-            b_trust_a_track[b].append(a)
+            outdegree[a] += 1
+            indegree[b] += 1
         for i in range(1, n + 1):
-            if len(b_trust_a_track[i]) == (n - 1) and not a_trust_b_track[i]:
+            if indegree[i] == (n - 1) and outdegree[i] == 0:
                 return i
         return -1
