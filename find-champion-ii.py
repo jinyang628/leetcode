e6681f348fbae19d345a4f290292670ff98239b1
Jin Yang <chenjinyang4192@gmail.com>
2025-07-01 06:14:20 +0100

    Another day another leetcode submission

--- find-champion-ii.py
+++ find-champion-ii.py
@@ -1,12 +1,14 @@
 class Solution:
     def findChampion(self, n: int, edges: List[List[int]]) -> int:
         in_order = defaultdict(int)
-        for _, element in edges:
-            in_order[element] += 1
-        champion = None
+        for a, b in edges:
+            in_order[b] += 1
+        winner = -1
         for i in range(n):
-            if not in_order[i]:
-                if champion is not None:
-                    return -1
-                champion = i
-        return champion
+            if in_order[i]:
+                continue
+            if winner == -1:
+                winner = i
+            else:
+                return -1
+        return winner
