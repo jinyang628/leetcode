876223e98918e5b55ee60d5a93bf722694d1c1bd
Jin Yang <chenjinyang4192@gmail.com>
2025-07-05 17:33:29 +0100

    Another day another leetcode submission

--- asteroid-collision.py
+++ asteroid-collision.py
@@ -1,23 +1,24 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         stack = []
-        for num in asteroids:
-            if num > 0:
-                stack.append(num)
-                continue 
-            exit = False
-            while not exit:
-                if not stack:
-                    stack.append(num)
-                    exit = True
-                elif stack[-1] < 0:
-                    stack.append(num)
-                    exit = True
-                elif stack[-1] == abs(num):
-                    stack.pop()
-                    exit = True
-                elif stack[-1] < abs(num):
-                    stack.pop()
-                else:
-                    exit = True
+        for asteroid in asteroids:
+            if asteroid > 0:
+                stack.append(asteroid)
+            elif not stack or stack[-1] < 0:
+                stack.append(asteroid)
+            else:
+                while stack:
+                    curr = stack[-1]
+                    if curr < 0:
+                        stack.append(asteroid)
+                        break
+                    elif stack[-1] > abs(asteroid):
+                        break
+                    else:
+                        stack.pop()
+                        if curr == abs(asteroid):
+                            break
+                        if not stack:
+                            stack.append(asteroid)
+                            break
         return stack
