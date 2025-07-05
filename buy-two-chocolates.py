31ce211d1d2aa406b5aca7685a79d0f2f534f4b3
Jin Yang <chenjinyang4192@gmail.com>
2025-07-05 18:14:31 +0100

    Another day another leetcode submission

--- buy-two-chocolates.py
+++ buy-two-chocolates.py
@@ -1,8 +1,8 @@
-import heapq
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
         heapq.heapify(prices)
-        lowest = heapq.heappop(prices)
-        second_lowest = heapq.heappop(prices)
-        total = lowest + second_lowest
-        return money - total if total <= money else money
+        first = heapq.heappop(prices)
+        second = heapq.heappop(prices)
+        if first + second > money:
+            return money
+        return money - first - second
