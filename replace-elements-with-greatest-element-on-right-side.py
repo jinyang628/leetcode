b48cfc16913615931313552cde8e8355721dccf1
Jin Yang <chenjinyang4192@gmail.com>
2025-07-06 11:21:09 +0100

    Another day another leetcode submission

--- replace-elements-with-greatest-element-on-right-side.py
+++ replace-elements-with-greatest-element-on-right-side.py
@@ -1,9 +1,9 @@
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
-        maxSoFar = 0
-        for i in range(len(arr) - 1, -1, -1):
-            tmp = maxSoFar
-            maxSoFar = max(arr[i], maxSoFar)
-            arr[i] = tmp
+        greatestSoFar = arr[-1]
         arr[-1] = -1
+        for i in range(len(arr) - 2, -1, -1):
+            original = arr[i]
+            arr[i] = greatestSoFar
+            greatestSoFar = max(greatestSoFar, original)
         return arr
