8dfee673492edb47982a9fbe3671849646207d8d
Jin Yang <chenjinyang4192@gmail.com>
2025-07-19 15:06:28 +0100

    Another day another leetcode submission

--- delete-node-in-a-bst.py
+++ delete-node-in-a-bst.py
@@ -10,18 +10,19 @@ class Solution:
             return root
         if root.val < key:
             root.right = self.deleteNode(root.right, key)
-            return root
         elif root.val > key:
             root.left = self.deleteNode(root.left, key)
-            return root
         else:
             if not root.left and not root.right:
                 return None
-            if not root.left or not root.left:
+            if root.left and root.right:
+                root.val = self.getMin(root.right)
+                root.right = self.deleteNode(root.right, root.val)
+            else:
                 return root.left if root.left else root.right
-            tmp = root.left
-            while tmp.right:
-                tmp = tmp.right
-            root.val = tmp.val
-            root.left = self.deleteNode(root.left, tmp.val)
-            return root
+        return root
+    def getMin(self, root: TreeNode) -> int:
+        while root:
+            last = root.val
+            root = root.left
+        return last
