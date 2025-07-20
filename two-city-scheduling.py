1310e135f831f52c0392168c6d208d659bef15f5
Jin Yang <chenjinyang4192@gmail.com>
2025-07-20 13:44:56 +0100

    Another day another leetcode submission

--- two-city-scheduling.py
+++ two-city-scheduling.py
@@ -1,13 +1,27 @@
+import heapq
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        arr = []
+        heap = [] # (negative_diff, favored_city (0,1), a_cost, b_cost)
         for a, b in costs:
-            arr.append((a - b,a, b))
-        arr.sort()
-        res = 0
-        for i in range(len(arr)):
-            if i < len(arr) // 2:
-                res += arr[i][1]
+            if a < b:
+                negative_diff = a - b
+                heapq.heappush(heap, (negative_diff, 0, a, b))
             else:
-                res += arr[i][2]
-        return res
+                negative_diff = b - a
+                heapq.heappush(heap, (negative_diff, 1, a, b))
+        a_costs = []
+        b_costs = []
+        max_length = len(costs) // 2
+        while heap:
+            _, favored_city, a_cost, b_cost = heapq.heappop(heap)
+            if not favored_city:
+                if len(a_costs) < max_length:
+                    a_costs.append(a_cost)
+                else:
+                    b_costs.append(b_cost)
+            else:
+                if len(b_costs) < max_length:
+                    b_costs.append(b_cost)
+                else:
+                    a_costs.append(a_cost)
+        return sum(a_costs) + sum(b_costs)
