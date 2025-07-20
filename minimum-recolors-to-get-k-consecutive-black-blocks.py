905132e003f95effc80758197428e87c4b03e607
Jin Yang <chenjinyang4192@gmail.com>
2025-07-20 13:29:22 +0100

    Another day another leetcode submission

--- minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -1,15 +1,16 @@
-class Solution:
-    def minimumRecolors(self, blocks: str, k: int) -> int:
-        left = right = currWhite = 0
-        minSoFar = float('inf')
-        while right < len(blocks):
-            if blocks[right] == "W":
-                currWhite += 1
-            right += 1
-            if right < k:
-                continue
-            minSoFar = min(minSoFar, currWhite)
-            if blocks[left] == "W":
-                currWhite -= 1
-            left += 1
-        return minSoFar
+"WWBBBWBWWBWWBBBWWWWWBWBBWBWBWBWBWBWBBBWBW"
+19
+"WBBBWWWWBBBWBWWBWWWWBWBBWBWWWBWWWWWWBWWWBBWB"
+34
+"WWBBBWWBBWWBBBBWWWWBWBBWWWBWWBBBBWBWWWWWBBBWBWBBBWBBWWWBWWWWBWWWBWBWWWWBWWBB"
+6
+"WBWBWBBWWWWBWBBBWBWBB"
+19
+"WBBBW"
+4
+"WWBWBWWBBWBWBWWBWWBWWBBBBWWBBWWWWBBWBWWWBBBBBWBWBWWWWBBWBBBBBWWBBBWBWWWBBBWWWWBBWWBWBB"
+40
+"B"
+1
+"BBWWWBBWBBBWWWWBBBBWWWWBBWWWBWBWWBBBWWBWWWB"
+2
