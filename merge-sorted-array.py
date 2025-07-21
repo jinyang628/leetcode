6cae63d0d0a71092fbd868761d809554a488b1e7
Jin Yang <chenjinyang4192@gmail.com>
2025-07-21 08:04:45 +0100

    Another day another leetcode submission

--- merge-sorted-array.py
+++ merge-sorted-array.py
@@ -2,22 +2,23 @@ class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         """
-        overall = len(nums1) - 1
-        second = len(nums2) - 1
-        first = overall - second - 1
-        while first >= 0 and second >= 0:
-            if nums1[first] >= nums2[second]:
-                nums1[overall] = nums1[first]
-                first -= 1
+        nums1_pointer = len(nums1) - 1
+        m -= 1
+        n -= 1
+        while m >= 0 and n >= 0:
+            if nums1[m] >= nums2[n]:
+                nums1[nums1_pointer] = nums1[m]
+                m -= 1
             else:
-                nums1[overall] = nums2[second]
-                second -= 1
-            overall -= 1
-        while first >= 0:
-            nums1[overall] = nums1[first]
-            first -= 1
-            overall -= 1
-        while second >= 0:
-            nums1[overall] = nums2[second]
-            second -= 1
-            overall -= 1
+                nums1[nums1_pointer] = nums2[n]
+                n -= 1
+            nums1_pointer -= 1
+        while m >= 0:
+            nums1[nums1_pointer] = nums1[m]
+            nums1_pointer -= 1
+            m -= 1
+        while n >= 0:
+            nums1[nums1_pointer] = nums2[n]
+            nums1_pointer -= 1
+            n -= 1
+        return nums1
