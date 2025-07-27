1e2a4c23e754de23c7d82b4a1123898f0c3aa338
Jin Yang <chenjinyang4192@gmail.com>
2025-07-27 16:30:32 +0100

    Another day another leetcode submission

--- baseball-game.py
+++ baseball-game.py
@@ -1,14 +1,1 @@
-class Solution:
-    def calPoints(self, operations: List[str]) -> int:
-        res = []
-        for operation in operations:
-            print(res)
-            if operation.isdigit() or operation[0] == "-":
-                res.append(int(operation))
-            elif operation == "+":
-                res.append(res[-2] + res[-1])
-            elif operation == "C":
-                res.pop()
-            elif operation == "D":
-                res.append(res[-1] * 2)
-        return sum(res)
+Deque<Integer> stack = new ArrayDeque();
