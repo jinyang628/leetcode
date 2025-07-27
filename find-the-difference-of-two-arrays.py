b13261412e12b12bde2c67022e12fe73323ef4bb
Jin Yang <chenjinyang4192@gmail.com>
2025-07-27 10:01:12 +0100

    Another day another leetcode submission

--- find-the-difference-of-two-arrays.py
+++ find-the-difference-of-two-arrays.py
@@ -1,14 +1,13 @@
-from collections import Counter
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        counter_one = set(nums1)
-        counter_two = set(nums2)
-        answer_one = []
-        answer_two = []
-        for element in counter_one:
-            if element not in counter_two:
-                answer_one.append(element)
-        for element in counter_two:
-            if element not in counter_one:
-                answer_two.append(element)
-        return [answer_one, answer_two]
+        nums1 = set(nums1)
+        nums2 = set(nums2)
+        first = set()
+        second = set()
+        for num in nums1:
+            if num not in nums2:
+                first.add(num)
+        for num in nums2:
+            if num not in nums1:
+                second.add(num)
+        return [list(first), list(second)]
