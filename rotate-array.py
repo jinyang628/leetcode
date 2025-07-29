593665e63ee03a7a0a95aed08bffd8f02fd6ed13
Jin Yang <chenjinyang4192@gmail.com>
2025-07-29 08:20:53 +0100

    Another day another leetcode submission

--- rotate-array.py
+++ rotate-array.py
@@ -3,22 +3,21 @@ class Solution:
         """
         """
         k %= len(nums)
-        pointer = len(nums) - k - 1
+        length = len(nums) - 1
+        mid = length - k
+        copy = mid
         left = 0
-        right = pointer
+        while left < copy:
+            nums[left], nums[copy] = nums[copy], nums[left]
+            left += 1
+            copy -= 1
+        copy = mid + 1
+        while copy < length:
+            nums[copy], nums[length] = nums[length], nums[copy]
+            copy += 1
+            length -= 1
+        left, right = 0, len(nums) - 1
         while left < right:
             nums[left], nums[right] = nums[right], nums[left]
             left += 1
-            right -= 1
-        left = pointer + 1
-        right = len(nums) - 1
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-            left += 1
-            right -= 1
-        left = 0
-        right = len(nums) - 1
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-            left +=  1
             right -= 1
