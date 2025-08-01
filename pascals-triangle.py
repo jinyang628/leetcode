5e4b70f3dded9a8480a5cdbc1daee43dae79fb55
Jin Yang <chenjinyang4192@gmail.com>
2025-08-01 08:10:34 +0100

    Another day another leetcode submission

--- pascals-triangle.py
+++ pascals-triangle.py
@@ -1,15 +1,15 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
+        res = []
         if numRows == 1:
             return [[1]]
         elif numRows == 2:
-            return [[1], [1,1]]
-        res = [[1], [1,1]]
+            return [[1], [1, 1]]
+        res = [[1], [1, 1]]
         for i in range(2, numRows):
-            prev = res[-1]
-            new = [1]
-            for i in range(len(prev) - 1):
-                new.append(prev[i] + prev[i + 1])
-            new.append(1)
-            res.append(new)
+            curr = [1]
+            for i in range(len(res[-1]) - 1):
+                curr.append(res[-1][i] + res[-1][i + 1])
+            curr.append(1)
+            res.append(curr)
         return res
