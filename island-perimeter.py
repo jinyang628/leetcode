1352eee0d324a52a6281130bc18460e983ca95c5
Jin Yang <chenjinyang4192@gmail.com>
2025-08-02 17:38:10 +0100

    Another day another leetcode submission

--- island-perimeter.py
+++ island-perimeter.py
@@ -5,18 +5,17 @@ class Solution:
         def dfs(r: int, c: int) -> int:
             if r < 0 or r >= row or c < 0 or c >= col:
                 return 1
-            if (r, c) in visited:
-                return 0
-            if grid[r][c] == 0:
+            if not grid[r][c]:
                 return 1
-            visited.add((r, c))
-            res = (
-                dfs(r - 1, c) + dfs(r + 1, c) + dfs(r, c - 1) + dfs(r, c + 1)
-            )
-            return res
+            if (r,c) in visited:
+                return 0
+            visited.add((r, c))   
+            left = dfs(r - 1, c) 
+            right = dfs(r + 1, c) 
+            top = dfs(r, c - 1) 
+            down = dfs(r, c + 1)
+            return left + right + top + down
         for i in range(row):
             for j in range(col):
-                if grid[i][j] == 0:
-                    continue
-                return dfs(i, j)
-        return 0
+                if grid[i][j]:
+                    return dfs(i, j)
