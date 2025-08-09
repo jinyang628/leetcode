ac474a8b514639c5e1e4f9756ba770a370a3c593
Jin Yang <chenjinyang4192@gmail.com>
2025-08-09 21:56:54 +0100

    Another day another leetcode submission

--- design-hashset.py
+++ design-hashset.py
@@ -1,14 +1,1 @@
-class MyHashSet:
-    def __init__(self):
-        self.track = [False] * (10 ** 6 + 1)
-    def add(self, key: int) -> None:
-        self.track[key] = True
-    def remove(self, key: int) -> None:
-        self.track[key] = False
-    def contains(self, key: int) -> bool:
-        return self.track[key]
-# Your MyHashSet object will be instantiated and called as such:
-# obj = MyHashSet()
-# obj.add(key)
-# obj.remove(key)
-# param_3 = obj.contains(key)
+class MyHashSet extends HashSet<Integer> {}
