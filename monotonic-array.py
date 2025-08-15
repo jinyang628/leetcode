89b4b4edf834099e572d43184833fdffb957a7d9
Jin Yang <chenjinyang4192@gmail.com>
2025-08-15 23:30:48 +0100

    Another day another leetcode submission

--- monotonic-array.py
+++ monotonic-array.py
@@ -1,14 +1,19 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
-        if len(nums) <= 1:
-            return True
-        res = True
-        for i in range(1,len(nums)):
-            if nums[i] > nums[i - 1]:
-                res = False
-        if res:
-            return res
-        for i in range(1,len(nums)):
-            if nums[i] < nums[i - 1]:
+        def isIncreasing(lst: list[int]) -> bool:
+            if len(lst) <= 1:
+                return True
+            for i in range(1, len(nums)):
+                if nums[i] >= nums[i - 1]:
+                    continue
                 return False
-        return True
+            return True
+        def isDecreasing(lst: list[int]) -> bool:
+            if len(lst) <= 1:
+                return True
+            for i in range(1, len(nums)):
+                if nums[i] <= nums[i - 1]:
+                    continue
+                return False
+            return True
+        return isIncreasing(nums) or isDecreasing(nums)
