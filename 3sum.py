f62a85fa7b13ce08aecc90cb39fc2303c20d1acc
Jin Yang <chenjinyang4192@gmail.com>
2025-08-18 22:24:22 +0100

    Another day another leetcode submission

--- 3sum.py
+++ 3sum.py
@@ -1,32 +1,25 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        length = len(nums)
-        i = 0
         res = []
-        while i < length - 2:
-            j = i + 1
-            k = length - 1
-            while j < k:
-                curr = nums[i] + nums[j] + nums[k]
-                if not curr:
-                    res.append([nums[i], nums[j] , nums[k]])
-                    j += 1
-                    while j < k and nums[j] == nums[j - 1]:
-                        j += 1
-                    k -= 1
-                    while j < k and nums[k] == nums[k + 1]:
-                        k -= 1
-                    continue
-                if curr < 0:
-                    j += 1
-                    while j < k and nums[j] == nums[j - 1]:
-                        j += 1
+        left = 0
+        while left < len(nums) - 2:
+            mid = left + 1
+            right = len(nums) - 1
+            while mid < right:
+                total = nums[left] + nums[mid] + nums[right]
+                if total == 0:
+                    res.append(
+                        [nums[left], nums[mid], nums[right]]
+                    )
+                    mid += 1
+                    while mid < right and nums[mid] == nums[mid - 1]:
+                        mid += 1
+                elif total < 0:
+                    mid += 1
                 else:
-                    k -= 1
-                    while j < k and nums[k] == nums[k + 1]:
-                        k -= 1
-            i += 1
-            while i < length - 2 and nums[i] == nums[i - 1]:
-                i += 1
+                    right -= 1
+            left += 1
+            while left < len(nums) - 2 and nums[left] == nums[left - 1]:
+                left += 1
         return res
