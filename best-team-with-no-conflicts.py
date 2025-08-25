e52ec6f6a0f74a862423bfd99b21f00b04798775
Jin Yang <chenjinyang4192@gmail.com>
2025-08-25 22:54:18 +0100

    Another day another leetcode submission

--- best-team-with-no-conflicts.py
+++ best-team-with-no-conflicts.py
@@ -1,7 +1,31 @@
-class Solution:
-    def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
-        dp = [0] * (1 + max(ages))
-        score_age = sorted(zip(scores, ages))
-        for score, age in score_age:
-            dp[age] = score + max(dp[:1+age])
-        return max(dp)
+class Solution {
+public:
+  int bestTeamScore(vector<int>& scores, vector<int>& ages) {
+      vector<vector<int>>v;
+      int n = scores.size();
+      int mx = 0;
+      for(int i=0;i<n;i++)
+      {
+          v.push_back({scores[i],ages[i]});
+          mx = max(mx,ages[i]);
+      }
+      sort(v.begin(),v.end());     
+      vector<vector<int>>dp(n,vector<int>(mx+1,-1));
+      return solve(0,0,v,dp);
+  }
+  int solve(int i, int maxAge,vector<vector<int>> &team,vector<vector<int>>& dp)
+  {
+      if(i == team.size()) return 0;
+      int &ret = dp[i][maxAge];
+      if(ret != -1) return ret;
+      // possible to take, so consider both options
+      if(team[i][1]>=maxAge)
+      {
+          ret = max(ret,team[i][0]+solve(i+1,team[i][1],team,dp)); // take
+          ret = max(ret,solve(i+1,maxAge,team,dp)); // don't take
+          return ret;
+      }
+      // otherwise just don't take
+      return ret = solve(i+1,maxAge,team,dp);
+  }
+};
