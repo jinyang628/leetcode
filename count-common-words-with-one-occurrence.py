185163e4ffb83e80e83d57795248c8440488a4f6
Jin Yang <chenjinyang4192@gmail.com>
2025-09-02 21:32:17 +0100

    Another day another leetcode submission

--- count-common-words-with-one-occurrence.py
+++ count-common-words-with-one-occurrence.py
@@ -1,14 +1,8 @@
-from collections import Counter
-class Solution:
-    def countWords(self, words1: List[str], words2: List[str]) -> int:
-        ones = Counter(words1)
-        twos = Counter(words2)
-        res = 0
-        for word, freq in ones.items():
-            if freq > 1:
-                continue
-            if word not in twos:
-                continue
-            if twos[word] == 1:
-                res += 1
-        return res
+for word in words2:
+        if seen.get(word, None) != None:
+            seen[word] -= 1
+    res = 0
+    for item in seen:
+        if seen[item] == 0:
+            res += 1
+    return res
