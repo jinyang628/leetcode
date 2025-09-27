b00e67cd4039520600f91006d61c8030b9fdaa16
Jin Yang <chenjinyang4192@gmail.com>
2025-09-27 23:12:42 +0100

    Another day another leetcode submission

--- design-a-food-rating-system.py
+++ design-a-food-rating-system.py
@@ -1,22 +1,22 @@
-import heapq
+from collections import defaultdict
 class FoodRatings:
     def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
-        self.food_to_cuisine = {}
-        self.cuisine_to_rating = defaultdict(list)
-        self.food_to_rating = {}
+        self.cuisines_tracker = defaultdict(list) # key is the cuisine name, value is a maxHeap of (rating, food)
+        self.food_to_cuisine = defaultdict(str)
+        self.ratings_tracker = {} # key is the food name, value is the current rating of that food
         for i in range(len(foods)):
+            self.ratings_tracker[foods[i]] = ratings[i]
             self.food_to_cuisine[foods[i]] = cuisines[i]
-            heapq.heappush(self.cuisine_to_rating[cuisines[i]], (-ratings[i], foods[i]))
-            self.food_to_rating[foods[i]] = -ratings[i]
+            heapq.heappush(self.cuisines_tracker[cuisines[i]], (-ratings[i], foods[i]))
     def changeRating(self, food: str, newRating: int) -> None:
+        self.ratings_tracker[food] = newRating
         cuisine = self.food_to_cuisine[food]
-        self.food_to_rating[food] = -newRating
-        heapq.heappush(self.cuisine_to_rating[cuisine], (-newRating, food))
+        heapq.heappush(self.cuisines_tracker[cuisine], (-newRating, food))
     def highestRated(self, cuisine: str) -> str:
         while True:
-            rating, food = self.cuisine_to_rating[cuisine][0]
-            if self.food_to_rating[food] != rating:
-                heapq.heappop(self.cuisine_to_rating[cuisine])
+            rating, food = self.cuisines_tracker[cuisine][0]
+            if self.ratings_tracker[food] != -rating:
+                heapq.heappop(self.cuisines_tracker[cuisine])
                 continue
             return food
 # Your FoodRatings object will be instantiated and called as such:
