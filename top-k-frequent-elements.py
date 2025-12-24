fe3c54ef195bd452f1452ec0dcbc7b325f7da2a1
Jin Yang <chenjinyang4192@gmail.com>
2025-12-24 19:42:57 +0000

    Another day another leetcode submission

--- top-k-frequent-elements.py
+++ top-k-frequent-elements.py
@@ -1,9 +1,16 @@
-import heapq
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = Counter(nums)
-        heap = []
-        for val, qty in count.items():
-            heap.append((-qty, val))
-        heapq.heapify(heap)
-        return [heapq.heappop(heap)[1] for _ in range(k)]
+1from collections import Counter
+2import heapq
+3class Solution(object):
+4    def topKFrequent(self, nums, k):
+5        """
+6789        """
+10        count = Counter(nums)
+11        heap = []
+12        for key, value in count.items():
+13            heap.append((-value, key))
+14        heapq.heapify(heap)
+15        res = []
+16        for _ in range(k):
+17            _, num = heapq.heappop(heap)
+18            res.append(num)
+19        return res
