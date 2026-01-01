093e543ade4c59b56a26402fb909e5ee78ae962f
Jin Yang <chenjinyang4192@gmail.com>
2026-01-01 08:12:32 +0800

    Another day another leetcode submission

--- best-time-to-buy-and-sell-stock.py
+++ best-time-to-buy-and-sell-stock.py
@@ -1,8 +1,11 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        minSoFar = float('inf')
-        res = 0
-        for price in prices:
-            minSoFar = min(minSoFar, price)
-            res = max(res, price - minSoFar)
-        return res
+1class Solution(object):
+2    def maxProfit(self, prices):
+3        """
+456        """
+7        maxSoFar = 0
+8        minSoFar = prices[0]
+9        for price in prices:
+10            minSoFar = min(minSoFar, price)
+11            maxSoFar = max(maxSoFar,price - minSoFar)
+1213        return maxSoFar
+1415
