d6369b91524dc0e0e0058e59a5ad7ee2c2830963
Jin Yang <chenjinyang4192@gmail.com>
2026-01-01 08:41:12 +0800

    Another day another leetcode submission

--- online-stock-span.py
+++ online-stock-span.py
@@ -1,15 +1,10 @@
+# Python
 class StockSpanner:
     def __init__(self):
-        # (price, date)
-        self.stack = deque([(inf, 0)])
-        self.date = 1
+        self.stack = []  # Each element: [price, span]
     def next(self, price: int) -> int:
-        while self.stack[-1][0] <= price:
-            self.stack.pop()
-        span = self.date - self.stack[-1][1]
-        self.stack.append((price, self.date))
-        self.date += 1
+        span = 1
+        while self.stack and self.stack[-1][0] <= price:
+            span += self.stack.pop()[1]
+        self.stack.append([price, span])
         return span
-# Your StockSpanner object will be instantiated and called as such:
-# obj = StockSpanner()
-# param_1 = obj.next(price)
