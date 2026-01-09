d005567dcdc821d664e8c0490de05b373e9bd08d
Jin Yang <chenjinyang4192@gmail.com>
2026-01-09 09:50:33 +0800

    Another day another leetcode submission

--- max-area-of-island.py
+++ max-area-of-island.py
@@ -1,17 +1,19 @@
-class Solution:
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        row, col = len(grid), len(grid[0])
-        def getIslandSize(r: int, c: int) -> int:
-            if r < 0 or c < 0 or r >= row or c >= col:
-                return 0
-            if grid[r][c] == 0 or grid[r][c] == -1:
-                return 0
-            grid[r][c] = -1
-            return 1 + getIslandSize(r - 1, c) + getIslandSize(r + 1, c) + getIslandSize(r, c - 1) + getIslandSize(r, c + 1) 
-        res = 0
-        for i in range(row):
-            for j in range(col):
-                if grid[i][j] == -1:
-                    continue
-                res = max(res, getIslandSize(i, j))
-        return res
+1class Solution:
+2    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+3        row, col = len(grid), len(grid[0])
+4        res = 0
+5        visited = set()
+67        def bfs(r: int, c: int) -> int:
+8            if r < 0 or r >= row or c < 0 or c >= col:
+9                return 0
+10            if (r, c) in visited:
+11                return 0
+12            if grid[r][c] == 0:
+13                return 0
+14            visited.add((r, c))
+15            return 1 + bfs(r - 1, c) + bfs(r + 1, c) + bfs(r, c - 1) + bfs(r, c + 1)
+1617        for i in range(row):
+18            for j in range(col):
+19                if grid[i][j] == 1 and (i, j) not in visited:
+20                    res = max(res, bfs(i, j))
+2122        return res
