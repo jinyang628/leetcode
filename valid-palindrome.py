3357d9506d01b0a4a1032a21c339165a4b5a0fbe
Jin Yang <chenjinyang4192@gmail.com>
2026-01-10 20:32:38 +0800

    Another day another leetcode submission

--- valid-palindrome.py
+++ valid-palindrome.py
@@ -1,24 +1,16 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        left = 0
-        right = len(s) -1
-        while left < right:
-            if not s[left].isalnum():
-                left += 1
-                continue
-            if not s[right].isalnum():
-                right -= 1
-                continue
-            if s[left].isdigit():
-                if s[left] != s[right]:
-                    return False
-                left += 1
-                right -= 1
-                continue 
-            if not s[right].isalpha():
-                return False
-            if s[left].lower() != s[right].lower():
-                return False
-            left += 1
-            right -=1
-        return True
+1class Solution:
+2    def isPalindrome(self, s: str) -> bool:
+3        left, right = 0, len(s) - 1
+45        while left < right:
+6            if not s[left].isalnum():
+7                left += 1
+8                continue
+9            if not s[right].isalnum():
+10                right -= 1
+11                continue
+12            if s[left].lower() != s[right].lower():
+13                return False
+14            left += 1
+15            right -= 1
+16        return True
+17
