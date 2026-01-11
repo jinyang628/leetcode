3c1e443adb0eff8f291e8b8ef6f8e83223257633
Jin Yang <chenjinyang4192@gmail.com>
2026-01-11 14:20:11 +0800

    Another day another leetcode submission

--- daily-temperatures.py
+++ daily-temperatures.py
@@ -1,14 +1,11 @@
-class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        length = len(temperatures)
-        if length <= 1:
-            return [0]
-        res = [0] * length
-        track = [(temperatures[0], 0)]
-        for i in range(1, len(temperatures)):
-            temp = temperatures[i]
-            while track and temp > track[-1][0]:
-                _, idx = track.pop()
-                res[idx] = i - idx 
-            track.append((temp, i))
-        return res
+1class Solution:
+2    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+3        res = [0] * len(temperatures)
+4        stack = []
+5        for idx, temperature in enumerate(temperatures):
+6            while stack and temperature > stack[-1][0]:
+7                _, prev_idx = stack.pop()
+8                res[prev_idx] = idx - prev_idx
+910            stack.append((temperature, idx))
+1112        return res
+131415
