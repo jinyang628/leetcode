2d4208c182dcee6b7dda014a7ff2c1724419fe07
Jin Yang <chenjinyang4192@gmail.com>
2026-01-12 15:55:41 +0800

    Another day another leetcode submission

--- permutations.py
+++ permutations.py
@@ -1,13 +1,14 @@
-class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) == 1:
-            return [[nums[0]]]
-        res = []
-        for _ in range(len(nums)):
-            curr = nums.pop(0)
-            wish = self.permute(nums)
-            for p in wish:
-                p.append(curr)
-            res.extend(wish)
-            nums.append(curr)
-        return res
+1class Solution:
+2    def permute(self, nums: List[int]) -> List[List[int]]:
+3        length = len(nums)
+4        if length == 1:
+5            return [[nums[0]]]
+6        res = []
+7        for _ in range(length):
+8            top = nums.pop(0)
+9            subsets = self.permute(nums)
+10            for subset in subsets:
+11                subset.append(top)
+12                res.append(subset)
+13            nums.append(top)
+14        return res
