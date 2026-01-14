19fa929e7b30d580a97ba6fd4128049e1779ea49
Jin Yang <chenjinyang4192@gmail.com>
2026-01-14 10:47:17 +0800

    Another day another leetcode submission

--- house-robber.py
+++ house-robber.py
@@ -1,7 +1,7 @@
-class Solution:
-    def rob(self, nums: List[int]) -> int:
-        dp = [0] * (len(nums) + 2) 
-        for i in range(len(nums)):
-            dp[i + 2] = max(dp[i + 1], nums[i] + dp[i])
-        print(dp)
-        return dp[-1]
+1class Solution:
+2    def rob(self, nums: List[int]) -> int:
+3        res = [0] * (len(nums) + 2)
+4        for i in range(2, len(res)):
+5            res[i] = max(nums[i - 2] + res[i - 2], res[i - 1])
+6        return res[-1]
+7
