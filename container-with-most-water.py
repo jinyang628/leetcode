317757b1dc2c088dddefeaaa85be4384a168f8cd
Jin Yang <chenjinyang4192@gmail.com>
2026-01-15 12:42:40 +0800

    Another day another leetcode submission

--- container-with-most-water.py
+++ container-with-most-water.py
@@ -1,12 +1,14 @@
-class Solution:
-    def maxArea(self, height: List[int]) -> int:
-        res = 0
-        left, right = 0, len(height) - 1
-        while left < right:
-            res = max(res, (right - left) * min(height[left], height[right]))
-            print(res)
-            if height[left] <= height[right]:
-                left += 1
-            else:
-                right -= 1
-        return res
+1class Solution:
+2    def maxArea(self, height: List[int]) -> int:
+3        left, right = 0, len(height) - 1
+4        maxSoFar = 0
+5        while left < right:
+6            curr = (right - left) * min(height[left], height[right])
+7            maxSoFar = max(
+8                maxSoFar, 
+910            )
+11            if height[left] <= height[right]:
+12                left += 1
+13            else:
+14                right -= 1
+15        return maxSoFar
