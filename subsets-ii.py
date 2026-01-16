9c0fd25cb664615c6937591b857e7dfe2cf35d85
Jin Yang <chenjinyang4192@gmail.com>
2026-01-16 10:24:20 +0800

    Another day another leetcode submission

--- subsets-ii.py
+++ subsets-ii.py
@@ -1,17 +1,19 @@
-class Solution:
-    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        res = []
-        def backtrack(idx: int, path: list[int]):
-            if idx == len(nums):
-                res.append(path[:])
-                return
-            path.append(nums[idx])
-            backtrack(idx + 1, path)
-            path.pop()
-            idx += 1
-            while idx < len(nums) and nums[idx] == nums[idx - 1]:
-                idx += 1
-            backtrack(idx, path)
-        backtrack(0, [])
-        return res
+1class Solution:
+2    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+3        nums.sort()
+4        res = []
+5        def dfs(idx: int, subset: list[int]) -> None:
+6            if idx == len(nums):
+7                res.append(subset[:])
+8                return
+910            # take
+11            subset.append(nums[idx])
+12            dfs(idx + 1, subset)
+13            subset.pop()
+1415            # leave
+16            idx += 1
+17            while idx < len(nums) and nums[idx] == nums[idx - 1]:
+18                idx += 1
+19            dfs(idx, subset)
+2021        dfs(0, [])
+22        return res
