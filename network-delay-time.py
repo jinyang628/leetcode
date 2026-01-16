ca619e4123d4e1cdda38d1354eef1d5967cd5a6b
Jin Yang <chenjinyang4192@gmail.com>
2026-01-16 10:41:15 +0800

    Another day another leetcode submission

--- network-delay-time.py
+++ network-delay-time.py
@@ -1,18 +1,24 @@
-from collections import defaultdict
-class Solution:
-    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        track = defaultdict(list)
-        for source, target, time in times:
-            track[source].append((target, time))
-        visited = set()
-        heap = [(0, k)]
-        while heap:
-            time, node = heapq.heappop(heap)
-            if node in visited:
-                continue 
-            visited.add(node)
-            if len(visited) == n:
-                return time
-            for target, distance in track[node]:
-                heapq.heappush(heap, (time + distance, target))
-        return -1
+1from collections import defaultdict
+2import heapq
+3class Solution:
+4    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+5        track = defaultdict(list)
+6        for u, v, w in times:
+7            track[u].append((v, w))
+8        visited = set()
+9        heap = [(0, k)]
+10        while heap:
+11            time, node = heapq.heappop(heap)
+12            if node in visited:
+13                continue
+14            visited.add(node)
+15            if len(visited) == n:
+16                return time
+17            for neighbor, dist in track[node]:
+18                if neighbor in visited:
+19                    continue
+20                heapq.heappush(heap, (dist + time, neighbor))
+21        if len(visited) == n:
+22            return time
+23        return -1
+2425
