626b98bbd9b8c479b04d0196d9cf56a07623b3b3
Jin Yang <chenjinyang4192@gmail.com>
2026-01-16 17:15:52 +0800

    Another day another leetcode submission

--- two-sum.py
+++ two-sum.py
@@ -1,8 +1,8 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        track = {}
-        for i in range(len(nums)):
-            curr = nums[i]
-            if curr in track:
-                return [track[curr], i]
-            track[target - curr] = i
+1class Solution:
+2    def twoSum(self, nums: List[int], target: int) -> List[int]:
+3        track = {} # key is the value we are looking for, value is the other index
+4        for idx, num in enumerate(nums):
+5            if num in track:
+6                return [idx, track[num]]
+7            track[target - num] = idx
+8
