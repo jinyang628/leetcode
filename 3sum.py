e0106892c8d31a3213eb561ec71734d10687f7b3
Jin Yang <chenjinyang4192@gmail.com>
2026-01-19 10:12:46 +0800

    Another day another leetcode submission

--- 3sum.py
+++ 3sum.py
@@ -1,25 +1,27 @@
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        res = []
-        left = 0
-        while left < len(nums) - 2:
-            mid = left + 1
-            right = len(nums) - 1
-            while mid < right:
-                total = nums[left] + nums[mid] + nums[right]
-                if total == 0:
-                    res.append(
-                        [nums[left], nums[mid], nums[right]]
-                    )
-                    mid += 1
-                    while mid < right and nums[mid] == nums[mid - 1]:
-                        mid += 1
-                elif total < 0:
-                    mid += 1
-                else:
-                    right -= 1
-            left += 1
-            while left < len(nums) - 2 and nums[left] == nums[left - 1]:
-                left += 1
-        return res
+1class Solution:
+2    def threeSum(self, nums: List[int]) -> List[List[int]]:
+3        nums.sort()
+4        left = 0
+5        res = []
+6        while left < len(nums) - 2:
+7            mid = left + 1
+8            right = len(nums) - 1
+9            while mid < right:
+10                curr_sum = nums[left] + nums[mid] + nums[right] 
+11                if curr_sum == 0:
+12                    res.append([nums[left], nums[mid], nums[right]])
+13                    mid += 1
+14                    right -= 1
+15                    while mid < right and nums[mid] == nums[mid - 1]:
+16                        mid += 1
+17                    while mid < right and nums[right] == nums[right + 1]:
+18                        right -= 1
+19                elif curr_sum < 0:
+20                    mid += 1
+21                else:
+22                    right -= 1
+2324            left += 1
+25            while left < len(nums) - 2 and nums[left] == nums[left - 1]:
+26                left += 1
+27        return res
+2829
