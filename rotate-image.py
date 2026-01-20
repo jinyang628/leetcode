bb180a3082aaf429a6f37efcb4bb8d547be601c1
Jin Yang <chenjinyang4192@gmail.com>
2026-01-20 14:35:01 +0800

    Another day another leetcode submission

--- rotate-image.py
+++ rotate-image.py
@@ -1,13 +1,14 @@
-class Solution:
-    def rotate(self, matrix: List[List[int]]) -> None:
-        """
-        """
-        top, down = 0, len(matrix) - 1
-        while top < down:
-            matrix[top], matrix[down] = matrix[down], matrix[top]
-            top += 1
-            down -= 1
-        row, col = len(matrix), len(matrix[0])
-        for i in range(row):
-            for j in range(i + 1, col):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+1class Solution:
+2    def rotate(self, matrix: List[List[int]]) -> None:
+3        """
+45        """
+6        row, col = len(matrix), len(matrix[0])
+7        top, bottom = 0, row - 1
+8        while top < bottom:
+9            matrix[top], matrix[bottom] = matrix[bottom], matrix[top]
+10            top += 1
+11            bottom -= 1
+12        for i in range(row):
+13            for j in range(i + 1, col):
+14                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+15
