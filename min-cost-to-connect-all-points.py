f6d9e665efe3ff566f6fea14423fdf7b8f6e9c3e
Jin Yang <chenjinyang4192@gmail.com>
2026-01-22 12:13:11 +0800

    Another day another leetcode submission

--- min-cost-to-connect-all-points.py
+++ min-cost-to-connect-all-points.py
@@ -1,40 +1,29 @@
-import math
-class UnionFind: 
-    def __init__(self, n: int):
-        self.parents = [i for i in range(n)]
-        self.rank = [1] * n
-    def find(self, a: int) -> int:
-        while self.parents[a] != a:
-            tmp = self.parents[a]
-            self.parents[a] = self.parents[tmp]
-            a = tmp
-        return a
-    def union(self, a: int, b: int):
-        if self.rank[a] <= self.rank[b]:
-            self.rank[b] += self.rank[a]
-            self.parents[self.find(a)] = self.find(b)
-        else:
-            self.rank[a] += self.rank[b]
-            self.parents[self.find(b)] = self.find(a)
-class Solution:
-    def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        def getDist(a: list[int], b: list[int]) -> int:
-            return abs(a[0] - b[0]) + abs(a[1] - b[1])
-        uf = UnionFind(len(points))
-        edges = []
-        for i in range(len(points) - 1):
-            for j in range(i + 1, len(points)):
-                edges.append((
-                    getDist(points[i], points[j]),
-                    i,
-                ))
-        edges.sort()
-        total = 0
-        for dist, a, b in edges:
-            if uf.rank[0] == len(points):
-                break
-            if uf.find(a) == uf.find(b):
-                continue 
-            uf.union(a, b)
-            total += dist
-        return total
+1class Solution:
+2    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+3        def getDist(first: list[int], second: list[int]) -> int:
+4            return abs(second[0] - first[0]) + abs(second[1] - first[1])
+5        tracker = defaultdict(list)
+6        for i in range(len(points) - 1):
+7            for j in range(i + 1, len(points)):
+8                dist = getDist(points[i], points[j])
+9                tracker[i].append(
+10                    (dist,j)
+11                )
+12                tracker[j].append(
+13                    (dist,i)
+14                )
+15        visited = set()
+16        heap = [(0, 0)]
+17        total_dist = 0
+18        while len(visited) != len(points):
+19            dist, point_idx = heapq.heappop(heap)
+20            if point_idx in visited:
+21                continue
+22            visited.add(point_idx)
+23            total_dist += dist
+24            for new_dist, new_point_idx in tracker[point_idx]:
+25                if new_point_idx in visited:
+26                    continue
+27                heapq.heappush(heap, (new_dist, new_point_idx))
+2829        return total_dist
+303132
