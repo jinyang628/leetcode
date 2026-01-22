de0c78d378000969769749f8a69b603e10a1f863
Jin Yang <chenjinyang4192@gmail.com>
2026-01-22 12:22:14 +0800

    Another day another leetcode submission

--- gas-station.py
+++ gas-station.py
@@ -1,20 +1,14 @@
-class Solution:
-    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        if sum(gas) < sum(cost):
-            return -1
-        remainingGas = idx = i = 0
-        while i < len(gas):
-            if remainingGas + gas[i] >= cost[i]:
-                remainingGas = remainingGas + gas[i] - cost[i]
-                i += 1
-            elif gas[i] >= cost[i]:
-                remainingGas = gas[i] - cost[i]
-                idx = i
-                i += 1
-            else:
-                while gas[i] < cost[i]:
-                    i += 1
-                remainingGas = gas[i] - cost[i]
-                idx = i
-                i += 1
-        return idx
+1class Solution:
+2    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+3        if sum(gas) < sum(cost):
+4            return -1
+5        curr = idx = last_idx = 0
+6        while idx < len(gas):
+7            curr += (gas[idx] - cost[idx])
+8            if curr < 0:
+9                last_idx = idx + 1
+10                idx = last_idx
+11                curr = 0
+12            else:
+13                idx += 1
+14        return last_idx
