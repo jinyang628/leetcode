cb09926991c5ee6b2935bc93767c91c1de9b354d
Jin Yang <chenjinyang4192@gmail.com>
2026-01-23 17:08:40 +0800

    Another day another leetcode submission

--- plus-one.py
+++ plus-one.py
@@ -1,16 +1,15 @@
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        carry = 1
-        for i in range(len(digits) - 1, -1, -1):
-            curr = digits[i]
-            new = curr + carry
-            if new == 10:
-                digits[i] = 0
-                carry = 1
-            else:
-                digits[i] = new
-                carry = 0
-        if carry == 1:
-            return [1] + digits
-        else:
-            return digits
+1class Solution:
+2    def plusOne(self, digits: List[int]) -> List[int]:
+3        carry = 1
+4        for i in range(len(digits) - 1, -1, -1):
+5            curr = digits[i] + carry
+6            if curr == 10:
+7                carry = 1
+8                curr = 0
+9            else:
+10                carry = 0
+11            digits[i] = curr
+12        if carry == 1:
+13            return [1] + digits
+14        return digits
+15
