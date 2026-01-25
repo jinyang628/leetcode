c815d56affa8c7f92071f5b76cb0a329008fac7b
Jin Yang <chenjinyang4192@gmail.com>
2026-01-25 16:08:53 +0800

    Another day another leetcode submission

--- sliding-window-maximum.py
+++ sliding-window-maximum.py
@@ -1,14 +1,20 @@
-from collections import deque
-class Solution:
-    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        queue = deque([])
-        res = []
-        for idx, num in enumerate(nums):
-            while queue and queue[-1] < num:
-                queue.pop()
-            queue.append(num)
-            if idx >= k and nums[idx - k] == queue[0]:
-                queue.popleft()
-            if idx >= (k - 1):
-                res.append(queue[0])
-        return res
+1from collections import deque
+2class Solution:
+3    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+4        if k == 1:
+5            return nums
+6        res = []
+7        queue = deque([]) # (num, idx)
+8        left = right = 0
+9        while right < len(nums):
+10            while len(queue) and nums[right] >= queue[-1][0]:
+11                queue.pop()
+12            queue.append((nums[right], right))
+13            right += 1
+14            if right >= k:
+15                res.append(queue[0][0])
+16                left += 1
+17                if queue[0][1] < left:
+18                    queue.popleft()
+19        return res
+202122
