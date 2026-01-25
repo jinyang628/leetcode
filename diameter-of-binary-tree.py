7a38f8d46f32a9f24b894e1f22d3bbb33851602c
Jin Yang <chenjinyang4192@gmail.com>
2026-01-25 21:09:39 +0800

    Another day another leetcode submission

--- diameter-of-binary-tree.py
+++ diameter-of-binary-tree.py
@@ -1,18 +1,24 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        max_diameter = [0]
-        def helper(node: Optional[TreeNode]) -> int:
-            if not node:
-                return 0
-            left = helper(node.left)
-            right = helper(node.right)
-            max_diameter[0] = max(max_diameter[0], left + right)
-            return max(left, right) + 1
-        helper(root)
-        return max_diameter[0]
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def getHeight(self, root: Optional[TreeNode]) -> int:
+9        if not root:
+10            return 0
+1112        left = self.getHeight(root.left)
+13        right = self.getHeight(root.right)
+14        return max(left, right) + 1
+1516    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+17        if not root:
+18            return 0
+19        left = self.diameterOfBinaryTree(root.left)
+20        right = self.diameterOfBinaryTree(root.right)
+21        return max(
+22            left,
+23            right,
+24            self.getHeight(root.left) + self.getHeight(root.right)
+25        )
+26
