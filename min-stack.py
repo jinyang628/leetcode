45cef345ff664546f6f6f61cc889fecca04047b2
Jin Yang <chenjinyang4192@gmail.com>
2026-01-25 16:27:44 +0800

    Another day another leetcode submission

--- min-stack.py
+++ min-stack.py
@@ -1,25 +1,20 @@
-class MinStack:
-    def __init__(self):
-        self.minTrack = [] # monotonically decreasing 
-        self.stack = []
-    def push(self, val: int) -> None:
-        self.stack.append(val)
-        if not self.minTrack:
-            self.minTrack.append(val)
-            return
-        if val <= self.minTrack[-1]:
-            self.minTrack.append(val)
-    def pop(self) -> None:
-        curr = self.stack.pop()
-        if curr == self.minTrack[-1]:
-            self.minTrack.pop()
-    def top(self) -> int:
-        return self.stack[-1]
-    def getMin(self) -> int:
-        return self.minTrack[-1]
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+1class MinStack:
+23    def __init__(self):
+4        self.stack = [] # (actual value, smallest_so_far)
+56    def push(self, val: int) -> None:
+7        if not self.stack:
+8            self.stack.append((val, val))
+9        else:
+10            self.stack.append((val, min(val, self.stack[-1][1])))
+1112    def pop(self) -> None:
+13        self.stack.pop()
+1415    def top(self) -> int:
+16        return self.stack[-1][0]
+171819    def getMin(self) -> int:
+20        return self.stack[-1][1]
+21222324# Your MinStack object will be instantiated and called as such:
+25# obj = MinStack()
+26# obj.push(val)
+27# obj.pop()
+28# param_3 = obj.top()
+29# param_4 = obj.getMin()
