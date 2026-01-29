1d79aba65fb516b30740e03912b1abf0c49568c4
Jin Yang <chenjinyang4192@gmail.com>
2026-01-29 10:42:28 +0800

    Another day another leetcode submission

--- validate-binary-search-tree.py
+++ validate-binary-search-tree.py
@@ -1,15 +1,15 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def isWithinBoundary(self, left: int, right: int, node: Optional[TreeNode]) -> bool:
-        if not node:
-            return True
-        if left < node.val < right:
-            return self.isWithinBoundary(left, node.val, node.left) and self.isWithinBoundary(node.val, right, node.right)
-        return False
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        return self.isWithinBoundary(float('-inf'), float('inf'), root)
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+9        def dfs(left: float, right: float, node: Optional[TreeNode]) -> bool:
+10            if not node:
+11                return True
+12            if left < node.val < right:
+13                return dfs(left, node.val, node.left) and dfs(node.val, right, node.right)
+14            return False
+1516        return dfs(float('-inf'), float('inf'), root)
