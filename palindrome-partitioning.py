8cc3070a586a678cc8f3d8f4333d94efb3cfdd69
Jin Yang <chenjinyang4192@gmail.com>
2026-01-29 19:26:37 +0800

    Another day another leetcode submission

--- palindrome-partitioning.py
+++ palindrome-partitioning.py
@@ -1,27 +1,30 @@
-class Solution:
-    def partition(self, s: str) -> List[List[str]]:
-        def isPalindrome(substring: str):
-            if not substring:
-                return False
-            left = 0
-            right = len(substring) - 1
-            while left < right:
-                if substring[left] == substring[right]:
-                    left += 1
-                    right -= 1
-                    continue
-                return False
-            return True
-        res = []
-        def backtrack(idx: int, path:list[list[int]]):
-            print(idx, path)
-            if idx == len(s):
-                res.append(path[:])
-                return 
-            for i in range(idx, len(s)):
-                if isPalindrome(s[idx: i + 1]):
-                    path.append(s[idx: i + 1])
-                    backtrack(i + 1, path)
-                    path.pop()
-        backtrack(0, [])
-        return res
+1class Solution:
+2    def partition(self, s: str) -> List[List[str]]:
+3        track = {} # key is f"{start}{end}", value is boolean indicating whether its terue. 
+4        def isPalindrome(left: int, right: int) -> bool:
+5            key = f"{left}{right}"
+6            if key in track:
+7                return track[key]
+8            substring = s[left: right + 1]
+9            l, r = 0, len(substring) - 1
+10            while l < r:
+11                if substring[l] != substring[r]:
+12                    track[key] = False
+13                    return False
+14                l += 1
+15                r -= 1
+16            track[key] = True
+17            return True
+1819        res = []
+20        def backtrack(left: int, right: int, path: list[str]) -> None:
+21            if right == len(s):
+22                if left == len(s):
+23                    res.append(path[:])
+24                return
+2526            if isPalindrome(left, right):
+27                path.append(s[left: right + 1])
+28                backtrack(right + 1, right + 1, path)
+29                path.pop()
+30            backtrack(left, right + 1, path) 
+3132        backtrack(0, 0, [])
+33        return res
