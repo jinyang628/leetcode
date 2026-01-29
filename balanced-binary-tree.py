de614a079338358b57390ccd19f48b42c3106670
Jin Yang <chenjinyang4192@gmail.com>
2026-01-29 11:22:44 +0800

    Another day another leetcode submission

--- balanced-binary-tree.py
+++ balanced-binary-tree.py
@@ -1,19 +1,2 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def getHeight(self, node: Optional[TreeNode]) -> int:
-        if not node:
-            return 0
-        leftHeight = self.getHeight(node.left)
-        rightHeight = self.getHeight(node.right)
-        return max(leftHeight, rightHeight) + 1
-    def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return True
-        leftHeight = self.getHeight(root.left)
-        rightHeight = self.getHeight(root.right)
-        return abs(rightHeight - leftHeight) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right)
+left height is 5.
+right height is 4.
