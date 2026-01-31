0322d482c56147771c535a1f4e14b4f6d3731ddb
Jin Yang <chenjinyang4192@gmail.com>
2026-01-31 16:02:11 +0800

    Another day another leetcode submission

--- happy-number.py
+++ happy-number.py
@@ -1,12 +1,12 @@
-class Solution:
-    def isHappy(self, n: int) -> bool:
-        track = set() # tracks the values seen so far
-        while n != 1:
-            if n in track:
-                return False
-            track.add(n)
-            new_num = 0
-            for c in str(n):
-                new_num += int(c) ** 2
-            n = new_num
-        return True
+1class Solution:
+2    def isHappy(self, n: int) -> bool:
+3        visited = set()
+4        while n not in visited:
+5            if n == 1:
+6                return True
+7            visited.add(n)
+8            new_number = 0
+9            for char in str(n):
+10                new_number += int(char) ** 2
+11            n = new_number
+12        return False
