0ef2e32a507b50d3c873ac609e9ab7a391f0e653
Jin Yang <chenjinyang4192@gmail.com>
2026-02-01 21:18:03 +0800

    Another day another leetcode submission

--- partition-labels.py
+++ partition-labels.py
@@ -1,31 +1,22 @@
-from collections import defaultdict
-class Solution:
-    def partitionLabels(self, s: str) -> List[int]:
-        if len(s) == 1:
-            return [1]
-        res = []
-        track = defaultdict(set) # key is the character, value is a set containing all the idxes it is present at
-        for i in range(len(s)):
-            curr = s[i]
-            track[curr].add(i)
-        track[s[0]].remove(0)
-        members = set()
-        if track[s[0]]:
-            members.add(s[0])
-        count = 1
-        for i in range(1, len(s)):
-            if not members:
-                res.append(count)
-                count = 0
-            count += 1
-            curr = s[i]
-            if curr in members:
-                track[curr].remove(i)
-                if not track[curr]:
-                    members.remove(curr)
-            else:
-                track[curr].remove(i)
-                if track[curr]:
-                    members.add(curr)
-        res.append(count)
-        return res
+1from collections import Counter
+23class Solution:
+4    def partitionLabels(self, s: str) -> List[int]:
+5        remainder = Counter(s)
+6        current = Counter()
+7        idx = counter = 0
+8        res = []
+9        while idx < len(s):
+10            counter += 1
+11            current[s[idx]] += 1
+12            remainder[s[idx]] -= 1
+13            is_valid_partition = True
+14            for key, freq in current.items():
+15                if remainder[key]: 
+16                    is_valid_partition = False
+17                    break
+18            if is_valid_partition:
+19                res.append(counter)
+20                counter = 0
+21                current = Counter()
+22            idx += 1
+23        return res
