898961a1f80a0c37c52eec97512bb0b2baf90aee
Jin Yang <chenjinyang4192@gmail.com>
2026-02-01 16:50:28 +0800

    Another day another leetcode submission

--- coin-change-ii.py
+++ coin-change-ii.py
@@ -1,18 +1,8 @@
 1class Solution:
 2    def change(self, amount: int, coins: List[int]) -> int:
-3        tracker = {}
-4        def backtrack(idx: int, remainder: int) -> int:
-5            if idx == len(coins):
-6                if not remainder:
-7                    return 1
-8                return 0
-9            if (idx, remainder) in tracker:
-10                return tracker[(idx, remainder)]
-11            if remainder < 0:
-12                return 0
-13            take = backtrack(idx, remainder - coins[idx])
-14            give = backtrack(idx + 1, remainder)
-15            tracker[(idx, remainder)] = take + give
-16            return take + give
-1718        return backtrack(0, amount)
-19
+3        res = [0] * (amount + 1)
+4        res[0] = 1
+5        for coin in coins:
+6            for i in range(coin, len(res)):
+7                res[i] += res[i - coin]
+89        return res[-1]
