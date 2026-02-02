6cdea727aa7fbd669ddf8911fef5a6abbac891ea
Jin Yang <chenjinyang4192@gmail.com>
2026-02-02 14:55:17 +0800

    Another day another leetcode submission

--- buy-two-chocolates.py
+++ buy-two-chocolates.py
@@ -1,8 +1,8 @@
-class Solution:
-    def buyChoco(self, prices: List[int], money: int) -> int:
-        heapq.heapify(prices)
-        first = heapq.heappop(prices)
-        second = heapq.heappop(prices)
-        if first + second > money:
-            return money
-        return money - first - second
+1class Solution:
+2    def buyChoco(self, prices: List[int], money: int) -> int:
+3        prices.sort()
+4        leftover = money - prices[0] - prices[1]
+5        if leftover >= 0:
+6            return leftover
+7        else:
+8            return money
