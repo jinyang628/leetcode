4afb199c46e0d38cd0266f4e455649b5e35ad95d
Jin Yang <chenjinyang4192@gmail.com>
2026-02-05 12:02:08 +0800

    Another day another leetcode submission

--- symmetric-tree.py
+++ symmetric-tree.py
@@ -1,29 +1,20 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-from collections import defaultdict, deque
-class Solution:
-    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        level_nodes = defaultdict(list)
-        queue = deque([])
-        queue.append((root, 1))
-        while queue:
-            node, level = queue.popleft()
-            if node:
-                level_nodes[level].append(node.val)
-                queue.append((node.left, level + 1))
-                queue.append((node.right, level + 1))
-            else:
-                level_nodes[level].append(-101)
-        for _, lst in level_nodes.items():
-            left = 0
-            right = len(lst) - 1
-            while left < right:
-                if lst[left] != lst[right]:
-                    return False
-                left += 1
-                right -= 1
-        return True
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def isSameTree(self, first: Optional[TreeNode], second: Optional[TreeNode]) -> bool:
+9        if not first and not second:
+10            return True
+11        if not first or not second:
+12            return False
+13        if first.val != second.val:
+14            return False
+15        return self.isSameTree(first.left, second.right) and self.isSameTree(first.right, second.left)
+1617    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+18        if not root:
+19            return True
+20        return self.isSameTree(root.left, root.right)
+21
