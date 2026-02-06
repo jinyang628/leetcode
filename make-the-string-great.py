55983df5d927437679a52f11f3b6f32ca3fbc996
Jin Yang <chenjinyang4192@gmail.com>
2026-02-06 10:49:15 +0800

    Another day another leetcode submission

--- make-the-string-great.py
+++ make-the-string-great.py
@@ -1,12 +1,8 @@
-class Solution:
-    def makeGood(self, s: str) -> str:
-        stack = []
-        for char in s:
-            if not stack:
-                stack.append(char)
-                continue
-            if char.lower() == stack[-1].lower() and char != stack[-1]:
-                stack.pop()
-            else:
-                stack.append(char)
-        return "".join(stack)
+"j"
+"I"
+"vV"
+"Nn"
+"nThYCFZCwfFzjNTsyYuUyYqvVStnxXfFnNdSsBQqPJNnwWnDdZfQqqFWwQqFXxIWXxchzfcGgDdjyHtziInNMEhHqQkKkwyQqgG"
+"qgEqEqeGQQeEGGqQggGeEqQgeEEQqQQqGgeEqeeEEqqeEgEeEQeqQqQEqQGgGQgGGgQQqgGeqQEeqQGgEeEEGGgQQqEeGgeEQqg"
+"orvhVjTTxNpVqfMjBvyIiIhHFfVbJwWmjJXWwDNjJFfkKNnQqQtvJgxXQEedcCPknXWtXtwSZzzZJLlWwvERHoOnNDdVROeEbBuU"
+"RERErEWrWrRRWRRRreweWeEWeEwerREeEeEWERwEreeEWrRrrwrrEewWRrRReERRwEeEewErRRwWwwWeEERERrEeRrrrReeErWww"
