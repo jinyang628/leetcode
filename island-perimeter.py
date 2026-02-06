946bf48ed000a6db5c8371e1b8b3ee6a90351197
Jin Yang <chenjinyang4192@gmail.com>
2026-02-06 14:10:02 +0800

    Another day another leetcode submission

--- island-perimeter.py
+++ island-perimeter.py
@@ -1,21 +1,17 @@
-class Solution:
-    def islandPerimeter(self, grid: List[List[int]]) -> int:
-        row, col = len(grid), len(grid[0])
-        visited = set()
-        def dfs(r: int, c: int) -> int:
-            if r < 0 or r >= row or c < 0 or c >= col:
-                return 1
-            if not grid[r][c]:
-                return 1
-            if (r,c) in visited:
-                return 0
-            visited.add((r, c))   
-            left = dfs(r - 1, c) 
-            right = dfs(r + 1, c) 
-            top = dfs(r, c - 1) 
-            down = dfs(r, c + 1)
-            return left + right + top + down
-        for i in range(row):
-            for j in range(col):
-                if grid[i][j]:
-                    return dfs(i, j)
+1class Solution:
+2    def islandPerimeter(self, grid: List[List[int]]) -> int:
+3        visited = set()
+4        row, col = len(grid), len(grid[0])
+5        def dfs(r: int, c: int) -> int:
+6            if r < 0 or r >= row or c < 0 or c >= col:
+7                return 1
+8            if grid[r][c] == 0:
+9                return 1
+10            if (r,c) in visited:
+11                return 0
+12            visited.add((r, c))
+1314            return dfs(r - 1, c) + dfs(r + 1, c) + dfs(r, c - 1) + dfs(r, c + 1)
+15        for i in range(row):
+16            for j in range(col):
+17                if grid[i][j] == 1:
+18                    return dfs(i, j)
