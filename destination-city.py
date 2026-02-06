448c65edefcfaa2ac9d64ecfd9459d8787d686d2
Jin Yang <chenjinyang4192@gmail.com>
2026-02-06 14:15:51 +0800

    Another day another leetcode submission

--- destination-city.py
+++ destination-city.py
@@ -1,11 +1,11 @@
-class Solution:
-    def destCity(self, paths: List[List[str]]) -> str:
-        track = {}
-        cities = set()
-        for src, dst in paths:
-            cities.add(src)
-            cities.add(dst)
-            track[src] = dst        
-        for city in cities:
-            if city not in track:
-                return city
+1class Solution:
+2    def destCity(self, paths: List[List[str]]) -> str:
+3        cities = set()
+4        outbound = defaultdict(list)
+5        for a, b in paths:
+6            cities.add(a)
+7            cities.add(b)
+8            outbound[a].append(b)
+910        for city in cities:
+11            if not outbound[city]:
+12                return city
