ba67e97e42e1c7a46da6394b97f163e47a0de1d0
Jin Yang <chenjinyang4192@gmail.com>
2026-02-09 17:33:46 +0800

    Another day another leetcode submission

--- score-of-a-string.py
+++ score-of-a-string.py
@@ -1,6 +1,6 @@
-class Solution:
-    def scoreOfString(self, s: str) -> int:
-        res = 0
-        for i in range(1, len(s)):
-            res += abs((ord(s[i]) - ord(s[i - 1])))
-        return res
+1class Solution:
+2    def scoreOfString(self, s: str) -> int:
+3        res = 0
+4        for i in range(len(s) - 1):
+5            res += abs(ord(s[i]) - ord(s[i + 1]))
+6        return res
