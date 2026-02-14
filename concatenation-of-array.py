dde4fab455879a19ed89994154522a6d912365ac
Jin Yang <chenjinyang4192@gmail.com>
2026-02-14 18:04:54 +0800

    Another day another leetcode submission

--- concatenation-of-array.py
+++ concatenation-of-array.py
@@ -1,5 +1,11 @@
-class Solution:
-    def getConcatenation(self, nums: List[int]) -> List[int]:
-        for i in range(len(nums)):
-            nums.append(nums[i])
-        return nums
+static int list_append(PyListObject *self, PyObject *v)
+{
+    if (self->allocated <= Py_SIZE(self)) {
+        if (list_resize(self, more) == -1)
+            return -1;
+    }
+    Py_INCREF(v);
+    Py_SET_SIZE(self, Py_SIZE(self)+1);
+    PyList_SET_ITEM(self, Py_SIZE(self)-1, v);
+    return 0;
+}
