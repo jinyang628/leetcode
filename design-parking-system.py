ba7f94e593b0d3de86adb5d06184b3d09d13ada6
Jin Yang <chenjinyang4192@gmail.com>
2026-02-14 21:48:51 +0800

    Another day another leetcode submission

--- design-parking-system.py
+++ design-parking-system.py
@@ -1,27 +1,24 @@
-class ParkingSystem:
-    def __init__(self, big: int, medium: int, small: int):
-        self.bigMax = big
-        self.mediumMax = medium
-        self.smallMax = small
-        self.big = 0
-        self.medium = 0
-        self.small = 0
-    def addCar(self, carType: int) -> bool:
-        if carType == 1:
-            if self.big == self.bigMax:
-                return False
-            self.big += 1
-            return True
-        if carType == 2:
-            if self.medium == self.mediumMax:
-                return False
-            self.medium += 1
-            return True
-        if carType == 3:
-            if self.small == self.smallMax:
-                return False
-            self.small += 1
-            return True
-# Your ParkingSystem object will be instantiated and called as such:
-# obj = ParkingSystem(big, medium, small)
-# param_1 = obj.addCar(carType)
+1class ParkingSystem:
+23    def __init__(self, big: int, medium: int, small: int):
+4        self.big = big
+5        self.medium = medium
+6        self.small = small
+78    def addCar(self, carType: int) -> bool:
+9        if carType == 1:
+10            if self.big >= 1:
+11                self.big -= 1
+12                return True
+13            return False
+14        if carType == 2:
+15            if self.medium >= 1:
+16                self.medium -= 1
+17                return True
+18            return False
+19        if carType == 3:
+20            if self.small >= 1:
+21                self.small -= 1
+22                return True
+23            return False
+242526# Your ParkingSystem object will be instantiated and called as such:
+27# obj = ParkingSystem(big, medium, small)
+28# param_1 = obj.addCar(carType)
