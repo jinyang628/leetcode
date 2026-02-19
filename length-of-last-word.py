a6ec76f4b9d84139d51feeb1b309829b15f98664
Jin Yang <chenjinyang4192@gmail.com>
2026-02-19 13:03:38 +0800

    Another day another leetcode submission

--- length-of-last-word.py
+++ length-of-last-word.py
@@ -1,12 +1,3 @@
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        res = 0
-        started = False
-        for i in range(len(s) - 1, -1, -1):
-            if s[i] == " ":
-                if started:
-                    return res
-            else:
-                started = True 
-                res += 1
-        return res
+1class Solution:
+2    def lengthOfLastWord(self, s: str) -> int:
+3        return len(s.strip().split(" ")[-1])
