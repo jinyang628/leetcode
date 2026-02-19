7ec04a0f9ee350e00e7b7fc74a70bc446597e012
Jin Yang <chenjinyang4192@gmail.com>
2026-02-19 13:15:32 +0800

    Another day another leetcode submission

--- append-characters-to-string-to-make-subsequence.py
+++ append-characters-to-string-to-make-subsequence.py
@@ -1,10 +1,12 @@
-class Solution:
-    def appendCharacters(self, s: str, t: str) -> int:
-        s_pointer = t_pointer = 0
-        while s_pointer < len(s) and t_pointer < len(t):
-            if s[s_pointer] == t[t_pointer]:
-                s_pointer += 1
-                t_pointer += 1
-            else:
-                s_pointer += 1
-        return len(t) - t_pointer
+"axbyc"
+"abc"
+"xyz"
+"xyzxyz"
+"abcxyz"
+"xyzabc"
+"abcdefgh"
+"hgfedcba"
+"abracadabra"
+"cadabra"
+"abcdefghijklmnopqrstuvwxyz"
+"abbbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbabbab"
