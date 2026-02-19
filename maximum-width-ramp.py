cfd409e02ca544d8e5ac7439a9f1eb80a58cb087
Jin Yang <chenjinyang4192@gmail.com>
2026-02-19 12:20:54 +0800

    Another day another leetcode submission

--- maximum-width-ramp.py
+++ maximum-width-ramp.py
@@ -1,14 +1,12 @@
 1class Solution:
 2    def maxWidthRamp(self, nums: List[int]) -> int:
-3        width = 0
-4        stack = []
-5        for i in range(len(nums)):
-6            if not stack or nums[stack[-1]] > nums[i]:
-7                stack.append(i)
-8        print(stack)
-9        for i in range(len(nums) - 1, -1, -1):
-10            while stack and nums[stack[-1]] <= nums[i]:
-11                idx = stack.pop()
-12                width = max(width, i - idx)
-13        return width
-14
+3        stack = []
+4        for i in range(len(nums)):
+5            if not stack or nums[stack[-1]] > nums[i]:
+6                stack.append(i)
+7        maxSoFar = 0
+8        for i in range(len(nums) - 1, -1, -1):
+9            while stack and nums[stack[-1]] <= nums[i]:
+10                maxSoFar = max(maxSoFar, i - stack[-1])
+11                stack.pop()
+12        return maxSoFar
