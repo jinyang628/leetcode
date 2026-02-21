97425efc0fcc1012ff36387a50ba10838f0a10b1
Jin Yang <chenjinyang4192@gmail.com>
2026-02-21 21:25:39 +0800

    Another day another leetcode submission

--- find-common-characters.py
+++ find-common-characters.py
@@ -1,17 +1,1 @@
-class Solution:
-    def commonChars(self, words: List[str]) -> List[str]:
-        if not words:
-            return []
-        counter = Counter(words[0])
-        for word in words[1:]:
-            tmp = Counter(word)
-            new_counter = Counter()
-            for key in counter:
-                if key not in tmp:
-                    continue
-                new_counter[key] = min(counter[key], tmp[key])
-            counter = new_counter
-        res = []
-        for key, freq in counter.items():
-            res.extend([key] * freq)
-        return res
+return reduce(lambda x, y: x & y, map(Counter, words)).elements()
