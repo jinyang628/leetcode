8d3921fb2b7e686b0a397eed909f1817da79e775
Jin Yang <chenjinyang4192@gmail.com>
2026-02-21 18:03:01 +0800

    Another day another leetcode submission

--- valid-palindrome-ii.py
+++ valid-palindrome-ii.py
@@ -1,20 +1,20 @@
-class Solution:
-    def validPalindrome(self, s: str) -> bool:
-        def isPalindrome(substring: str) -> bool:
-            if not substring:
-                return True
-            l, r = 0, len(substring) - 1
-            while l < r:
-                if substring[l] == substring[r]:
-                    l += 1
-                    r -= 1
-                    continue
-                return False
-            return True
-        left, right = 0, len(s) - 1
-        while left < right:
-            if s[left] != s[right]:
-                return isPalindrome(s[left: right]) or isPalindrome(s[left + 1: right + 1])
-            left += 1
-            right -= 1
-        return True
+1class Solution:
+2    def validPalindrome(self, s: str) -> bool:
+3        def isPalindrome(substr: str) -> bool:
+4            left, right = 0, len(substr) - 1
+5            while left < right:
+6                if substr[left] != substr[right]:
+7                    return False
+8                left += 1
+9                right -= 1
+10            return True
+11        left, right = 0, len(s) - 1
+12        seen_diff = False
+13        while left < right:
+14            if s[left] != s[right]:
+15                if seen_diff:
+16                    return False
+17                return isPalindrome(s[left:right]) or isPalindrome(s[left + 1: right + 1])
+18            left += 1
+19            right -= 1
+20        return True
