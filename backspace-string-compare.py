e01d4b173d9faadc16cfd259bf5c51148a3b3084
Jin Yang <chenjinyang4192@gmail.com>
2026-02-21 11:31:47 +0800

    Another day another leetcode submission

--- backspace-string-compare.py
+++ backspace-string-compare.py
@@ -1,17 +1,17 @@
-class Solution:
-    def backspaceCompare(self, s: str, t: str) -> bool:
-        s_stack = []
-        t_stack = []
-        for char in s:
-            if char == "#":
-                if s_stack:
-                    s_stack.pop()
-            else:
-                s_stack.append(char)
-        for char in t:
-            if char == "#":
-                if t_stack:
-                    t_stack.pop()
-            else:
-                t_stack.append(char)
-        return "".join(s_stack) == "".join(t_stack)
+1class Solution:
+2    def backspaceCompare(self, s: str, t: str) -> bool:
+3        s_stack = []
+4        t_stack = []
+5        for char in s:
+6            if char == "#":
+7                if s_stack:
+8                    s_stack.pop()
+9            else:
+10                s_stack.append(char)
+11        for char in t:
+12            if char == "#":
+13                if t_stack:
+14                    t_stack.pop()
+15            else:
+16                t_stack.append(char)
+1718        return "".join(s_stack) == "".join(t_stack)
