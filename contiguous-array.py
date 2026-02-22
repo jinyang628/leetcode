8ee10ec76567284cebfa2b48835deac61d80ec7e
Jin Yang <chenjinyang4192@gmail.com>
2026-02-22 19:58:32 +0800

    Another day another leetcode submission

--- contiguous-array.py
+++ contiguous-array.py
@@ -1,13 +1,9 @@
-class Solution:
-    def findMaxLength(self, nums: List[int]) -> int:
-        prefixSums = {}
-        total = res = 0
-        for i in range(len(nums)):
-            total += 1 if nums[i] == 1 else -1
-            if not total:
-                res = i + 1
-            elif total in prefixSums:
-                res = max(res, i - prefixSums[total])
-            else:
-                prefixSums[total] = i
-        return res
+[0, 1, 1, 1, 1, 1, 0, 1, 0, 1]
+[1, 1, 0, 0, 1, 1, 0, 1, 0, 1]
+[0, 0, 0, 1, 1, 0, 0, 1, 1, 0]
+[1, 0, 1, 1, 0, 1, 1, 1, 0, 1]
+[0, 0, 0, 1, 1, 0, 1, 1, 1, 1]
+[1, 0, 0, 0, 1, 1, 0, 1, 1, 0]
+[0, 0, 1, 0, 0, 1, 1, 1, 0, 1]
+[0, 1, 0, 1, 0, 0, 1, 0, 0, 0]
+[1, 1, 1, 1, 1, 1, 1, 1]
