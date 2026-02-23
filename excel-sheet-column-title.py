21d15bef41876aad1d72800829578619c4596c26
Jin Yang <chenjinyang4192@gmail.com>
2026-02-23 23:25:02 +0800

    Another day another leetcode submission

--- excel-sheet-column-title.py
+++ excel-sheet-column-title.py
@@ -1,8 +1,10 @@
-class Solution:
-    def convertToTitle(self, columnNumber: int) -> str:
-        res = ""
-        while columnNumber > 0:
-            columnNumber -= 1
-            res = chr((columnNumber % 26) + ord("A")) + res
-            columnNumber //= 26
-        return res
+1class Solution:
+2    def convertToTitle(self, columnNumber: int) -> str:
+3        res = []
+4        while columnNumber:
+5            columnNumber -= 1
+6            res.append(
+7                chr(ord('A') + columnNumber % 26)
+8            )
+9            columnNumber //= 26
+10        return "".join(reversed(res))
