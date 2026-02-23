75ba0f9566e28646f8d0ca820c242a3d990a03fe
Jin Yang <chenjinyang4192@gmail.com>
2026-02-23 16:03:09 +0800

    Another day another leetcode submission

--- basic-calculator-ii.py
+++ basic-calculator-ii.py
@@ -1,40 +1,20 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        i = 0
-        curr = ""
-        stack = []
-        while i < len(s):
-            if s[i] == " ":
-                pass
-            elif s[i].isdigit():
-                curr += s[i]
-            else:
-                stack.append(int(curr))
-                curr = ""
-                stack.append(s[i])
-            i += 1
-        stack.append(int(curr))
-        new_stack = []
-        i = 0
-        while i < len(stack):
-            if stack[i] == "*":
-                new_stack.append(
-                    new_stack.pop() * stack[i + 1]
-                )
-                i += 2
-            elif stack[i] == "/":
-                new_stack.append(
-                    math.floor(new_stack.pop() / stack[i + 1])
-                )
-                i += 2
-            else:
-                new_stack.append(stack[i])
-                i += 1
-        base = new_stack[0]
-        for i in range(1, len(new_stack), 2):
-            operator, digit = new_stack[i], new_stack[i + 1]
-            if operator == "+":
-                base += digit
-            else:
-                base -= digit
-        return base
+        inner, outer, result, opt = 0, 0, 0, '+'
+        for c in s + '+':
+            if c == ' ': continue
+            if c.isdigit():
+                inner = 10 * inner + int(c)
+                continue
+            if opt == '+':
+                result += outer
+                outer = inner
+            elif opt == '-':
+                result += outer
+                outer = -inner
+            elif opt == '*':
+                outer = outer * inner
+            elif opt == '/':
+                outer = int(outer / inner)
+            inner, opt = 0, c
+        return result + outer
