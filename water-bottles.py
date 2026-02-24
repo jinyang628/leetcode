9ff6f61bfee25b471619eb977714b7e733a7518c
Jin Yang <chenjinyang4192@gmail.com>
2026-02-24 10:36:56 +0800

    Another day another leetcode submission

--- water-bottles.py
+++ water-bottles.py
@@ -1,9 +1,3 @@
 class Solution:
     def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
-        res = carry = 0
-        while numBottles:
-            res += numBottles
-            original = numBottles
-            numBottles = (numBottles + carry) // numExchange
-            carry = (original + carry) % numExchange
-        return res
+        return numBottles + (numBottles-1)//(numExchange-1)
