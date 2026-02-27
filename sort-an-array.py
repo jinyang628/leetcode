a1a5f9e1a46b3f3d4aa4a10ab3c0b7c90dc37bb7
Jin Yang <chenjinyang4192@gmail.com>
2026-02-27 12:07:46 +0800

    Another day another leetcode submission

--- sort-an-array.py
+++ sort-an-array.py
@@ -1,30 +1,30 @@
-from random import randint
-def partition(array: list[int], low: int, high: int) -> int:
-    # this is function for choosing pivot and partitate the array
-    pivot: int = array[randint(low, high)]
-    write_ind_smaller: int = low  # where to write elements smaller than pivot
-    write_ind_greater: int = high  # where to write elements greater than pivot
-    cur = low
-    while cur < write_ind_greater + 1:
-        num = array[cur]
-        if num < pivot:
-            array[write_ind_smaller], array[cur] = array[cur], array[write_ind_smaller]
-            write_ind_smaller += 1
-            cur += 1
-        elif num > pivot:
-            array[write_ind_greater], array[cur] = array[cur], array[write_ind_greater]
-            write_ind_greater -= 1
-        else:
-            cur += 1
-    return write_ind_smaller  # return index of the pivot
-def quick_sort(array, low, high) -> None:  # [low, high]
-    # Base case: if range is empty or contains only 1 element
-    if low >= high:
-        return
-    pivot_ind: int = partition(array, low, high)
-    quick_sort(array, low, pivot_ind - 1)  # left part
-    quick_sort(array, pivot_ind + 1, high)  # right part
-class Solution:
-    def sortArray(self, nums: List[int]) -> List[int]:
-        quick_sort(nums, 0, len(nums) - 1)
-        return nums
+1class Solution:
+2    def merge(self, arr1: list[int], arr2: list[int]) -> list[int]:
+3        if not arr1:
+4            return arr2
+5        if not arr2:
+6            return arr1
+7        res = []
+8        counter1 = counter2 = 0
+9        while counter1 < len(arr1) and counter2 < len(arr2):
+10            if arr1[counter1] <= arr2[counter2]:
+11                res.append(arr1[counter1])
+12                counter1 += 1
+13            else:
+14                res.append(arr2[counter2])
+15                counter2 += 1
+1617        while counter1 < len(arr1):
+18            res.append(arr1[counter1])
+19            counter1 += 1
+20        while counter2 < len(arr2):
+21            res.append(arr2[counter2])
+22            counter2 += 1
+23        return res
+2425    def sortArray(self, nums: List[int]) -> List[int]:
+26        if len(nums) <= 1:
+27            return nums
+28        middleIdx = len(nums) // 2
+29        left = self.sortArray(nums[:middleIdx])
+30        right = self.sortArray(nums[middleIdx:])
+31        return self.merge(left, right)
+32
