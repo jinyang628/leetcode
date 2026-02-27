0594ed42b17b2ddb7fa7cfa271529e3796190ef9
Jin Yang <chenjinyang4192@gmail.com>
2026-02-27 11:50:40 +0800

    Another day another leetcode submission

--- replace-elements-with-greatest-element-on-right-side.py
+++ replace-elements-with-greatest-element-on-right-side.py
@@ -1,9 +1,13 @@
-class Solution:
-    def replaceElements(self, arr: List[int]) -> List[int]:
-        greatestSoFar = arr[-1]
-        arr[-1] = -1
-        for i in range(len(arr) - 2, -1, -1):
-            original = arr[i]
-            arr[i] = greatestSoFar
-            greatestSoFar = max(greatestSoFar, original)
-        return arr
+1from collections import deque
+2class Solution:
+3    def replaceElements(self, arr: List[int]) -> List[int]:
+4        largestSoFar = deque([])
+5        maxSoFar = arr[-1]
+6        for num in arr[::-1]:
+7            maxSoFar = max(maxSoFar, num)
+8            largestSoFar.appendleft(maxSoFar)
+9        res = []
+10        for i in range(len(arr) - 1):
+11            res.append(largestSoFar[i + 1])
+12        res.append(-1)
+13        return res
