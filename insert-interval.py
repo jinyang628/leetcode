c009d484e41734dfd226aeb1ad3e061f3cd9aca7
Jin Yang <chenjinyang4192@gmail.com>
2026-02-27 21:20:16 +0800

    Another day another leetcode submission

--- insert-interval.py
+++ insert-interval.py
@@ -1,17 +1,16 @@
-class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        if not intervals:
-            return [newInterval]
-        n = len(intervals)
-        res = []
-        i = 0
-        while i < n and intervals[i][1] < newInterval[0]:
-            res.append(intervals[i])
-            i += 1
-        while i < n and intervals[i][0] <= newInterval[1]:
-            newInterval[0] = min(newInterval[0], intervals[i][0])
-            newInterval[1] = max(newInterval[1], intervals[i][1])
-            i += 1
-        res.append(newInterval)
-        res.extend(intervals[i:])
-        return res
+[]
+[2,5]
+[[3,5],[8,10]]
+[1,2]
+[[1,3],[6,9]]
+[11,12]
+[[1,3],[6,9]]
+[2,5]
+[[1,2],[3,5],[8,10]]
+[2,9]
+[[1,3],[6,9]]
+[2,7]
+[[1,3],[6,9]]
+[4,7]
+[[1,3],[6,9]]
+[3,6]
