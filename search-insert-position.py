a75edd5e799ad09833d0539b12b2270a03a58eba
Jin Yang <chenjinyang4192@gmail.com>
2026-02-28 19:41:07 +0800

    Another day another leetcode submission

--- search-insert-position.py
+++ search-insert-position.py
@@ -1,13 +1,12 @@
-class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        left = 0
-        right = len(nums) - 1
-        while left <= right:
-            mid = left + (right - left) // 2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
-                left = mid + 1
-            else:
-                right = mid - 1
-        return left
+1class Solution:
+2    def searchInsert(self, nums: List[int], target: int) -> int:
+3        left, right = 0, len(nums) - 1
+4        while left <= right:
+5            mid = left + (right - left) // 2
+6            if nums[mid] == target:
+7                return mid
+8            elif nums[mid] < target:
+9                left = mid + 1
+10            else:
+11                right = mid - 1
+12        return left
