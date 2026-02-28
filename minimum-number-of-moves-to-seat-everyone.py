d576700dbaaa7f47f3530631538faf04d3d8b54e
Jin Yang <chenjinyang4192@gmail.com>
2026-02-28 12:25:13 +0800

    Another day another leetcode submission

--- minimum-number-of-moves-to-seat-everyone.py
+++ minimum-number-of-moves-to-seat-everyone.py
@@ -1,8 +1,8 @@
-class Solution:
-    def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
-        seats.sort()
-        students.sort()
-        res = 0
-        for i in range(len(seats)):
-            res += abs(seats[i] - students[i])
-        return res
+1class Solution:
+2    def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+3        seats.sort()
+4        students.sort()
+5        res = 0
+6        for i in range(len(seats)):
+7            res += abs(seats[i] - students[i])
+8        return res
