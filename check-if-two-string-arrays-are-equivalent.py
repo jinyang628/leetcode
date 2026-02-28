4c12f1cc10e240deeb09f92a7dceb754f22f1edb
Jin Yang <chenjinyang4192@gmail.com>
2026-02-28 17:55:29 +0800

    Another day another leetcode submission

--- check-if-two-string-arrays-are-equivalent.py
+++ check-if-two-string-arrays-are-equivalent.py
@@ -1,3 +1,3 @@
-class Solution:
-    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
-        return "".join(word1) == "".join(word2)
+1class Solution:
+2    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+3        return "".join(word1) == "".join(word2)
