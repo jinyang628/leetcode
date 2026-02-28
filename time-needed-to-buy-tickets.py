3de99b3b551666cee0256b6b52804e72e6042836
Jin Yang <chenjinyang4192@gmail.com>
2026-02-28 16:22:45 +0800

    Another day another leetcode submission

--- time-needed-to-buy-tickets.py
+++ time-needed-to-buy-tickets.py
@@ -1,10 +1,3 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        turns_required = tickets[k]
-        res = 0
-        for i in range(len(tickets)):
-            if i <= k:
-                res += min(tickets[i], turns_required)
-            else:
-                res += min(tickets[i], turns_required - 1)
-        return res
+        return sum(min(tickets[k] - (i > k), num) for i,num in enumerate(tickets))
