a66fc472834e9de74fd95f84eb32895dd0bc1fa5
Jin Yang <chenjinyang4192@gmail.com>
2026-03-02 23:01:42 +0800

    Another day another leetcode submission

--- rotate-array.py
+++ rotate-array.py
@@ -1,23 +1,22 @@
-class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        """
-        """
-        k %= len(nums)
-        length = len(nums) - 1
-        mid = length - k
-        copy = mid
-        left = 0
-        while left < copy:
-            nums[left], nums[copy] = nums[copy], nums[left]
-            left += 1
-            copy -= 1
-        copy = mid + 1
-        while copy < length:
-            nums[copy], nums[length] = nums[length], nums[copy]
-            copy += 1
-            length -= 1
-        left, right = 0, len(nums) - 1
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-            left += 1
-            right -= 1
+1class Solution:
+2    def rotate(self, nums: List[int], k: int) -> None:
+3        """
+45        """
+6        k =  k % len(nums)
+7        mid = len(nums) - k
+8        left, right = 0, len(nums) - 1
+9        left_mid_pointer = mid - 1
+10        while left < left_mid_pointer:
+11            nums[left], nums[left_mid_pointer] = nums[left_mid_pointer], nums[left]
+12            left += 1
+13            left_mid_pointer -= 1
+14        while mid < right:
+15            nums[mid], nums[right] = nums[right], nums[mid]
+16            mid += 1
+17            right -= 1
+18        left, right = 0, len(nums) - 1
+19        while left < right:
+20            nums[left], nums[right] = nums[right], nums[left]
+21            left += 1
+22            right -= 1
+23
