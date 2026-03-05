a20ca59f819ffd67429c674b62a99f2f236a3729
Jin Yang <chenjinyang4192@gmail.com>
2026-03-05 10:41:13 +0800

    Another day another leetcode submission

--- find-pivot-index.py
+++ find-pivot-index.py
@@ -1,11 +1,15 @@
-class Solution:
-    def pivotIndex(self, nums: List[int]) -> int:
-        left, right = 0, sum(nums) - nums[0]
-        for i in range(len(nums) - 1):
-            if left == right:
-                return i
-            left += nums[i]
-            right -= nums[i + 1]
-        if left == right:
-            return len(nums) - 1
-        return -1
+1class Solution:
+2    def pivotIndex(self, nums: List[int]) -> int:
+3        left_sum = 0
+4        right_sum = sum(nums) - nums[0]
+5        idx = 0
+6        while idx < len(nums):
+7            if left_sum == right_sum:
+8                return idx
+9            idx += 1
+10            left_sum += nums[idx - 1]
+11            if idx == len(nums):
+12                break
+13            right_sum -= nums[idx]
+14        return -1
+15
