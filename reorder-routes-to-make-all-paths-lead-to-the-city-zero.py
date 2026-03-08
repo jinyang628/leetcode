e13dcdeee681eca13adc1f81406ae72b47ef11fc
Jin Yang <chenjinyang4192@gmail.com>
2026-03-08 18:50:17 +0800

    Another day another leetcode submission

--- reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,17 +1,21 @@
-class Solution:
-    def dfs(self, adj: List[List[Tuple[int, int]]], visited: List[bool], minChange: List[int], currCity: int) -> None:
-        visited[currCity] = True
-        for neighbourCity in adj[currCity]:
-            if not visited[neighbourCity[0]]:
-                if neighbourCity[1] == 1:
-                    minChange[0] += 1
-                self.dfs(adj, visited, minChange, neighbourCity[0])
-    def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        adj = [[] for _ in range(n)]
-        for connection in connections:
-            adj[connection[0]].append((connection[1], 1))
-            adj[connection[1]].append((connection[0], -1))
-        visited = [False] * n
-        minChange = [0]
-        self.dfs(adj, visited, minChange, 0)
-        return minChange[0]
+1class Solution:
+2    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+3        res = [0]
+4        neighbors = defaultdict(list)
+5        for a, b in connections:
+6            neighbors[a].append((b, False))
+7            neighbors[b].append((a, True))
+8        visited = set()
+9        def dfs(node: int) -> None:
+10            if node in visited:
+11                return
+12            visited.add(node)
+13            for neighbor, is_inward in neighbors[node]:
+14                if neighbor in visited:
+15                    continue
+16                if not is_inward:
+17                    res[0] = res[0] + 1
+18                dfs(neighbor)
+1920        dfs(0)
+21        return res[0]
+2223
