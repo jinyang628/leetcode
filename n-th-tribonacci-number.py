d0d5947d65c3ca27ab1f2c289f8ca2efaa00e31e
Jin Yang <chenjinyang4192@gmail.com>
2026-03-09 20:53:44 +0800

    Another day another leetcode submission

--- n-th-tribonacci-number.py
+++ n-th-tribonacci-number.py
@@ -1,15 +1,8 @@
-class Solution:
-    def tribonacci(self, n: int) -> int:
-        if not n:
-            return 0
-        first = 0
-        second = 1
-        third = 1
-        curr = 2
-        while curr < n:
-            new = first + second + third
-            first = second
-            second = third
-            third = new
-            curr += 1
-        return third
+1class Solution:
+2    def tribonacci(self, n: int) -> int:
+3        dp = [0, 1, 1]
+4        for i in range(3, n + 1):
+5            dp.append(
+6                dp[-1] + dp[-2] + dp[-3]
+7            )
+8        return dp[n]
