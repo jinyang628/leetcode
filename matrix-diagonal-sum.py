57921a4c906cb169681efc15b5827c579c25734d
Jin Yang <chenjinyang4192@gmail.com>
2026-03-11 19:49:31 +0800

    Another day another leetcode submission

--- matrix-diagonal-sum.py
+++ matrix-diagonal-sum.py
@@ -1,6 +1,11 @@
-public int diagonalSum(int[][] mat) {
-    int n = mat.length;
-    return IntStream.range(0, n)
-            .map(i -> mat[i][i] + mat[i][n-1-i])
-            .sum() - (n % 2 == 1 ? mat[n/2][n/2] : 0);
-}
+1class Solution:
+2    def diagonalSum(self, mat: List[List[int]]) -> int:
+3        row = col = len(mat)
+4        total = 0
+5        if row % 2 == 1:
+6            total -= mat[row // 2][col // 2]
+7        for i in range(row):
+8            total += mat[i][i]
+9            total += mat[i][row - i - 1]
+10        return total
+11
