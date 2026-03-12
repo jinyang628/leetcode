f2a3ae0f49fbd3a2562b6e7b5635b420b3d3c276
Jin Yang <chenjinyang4192@gmail.com>
2026-03-12 16:58:29 +0800

    Another day another leetcode submission

--- shift-2d-grid.py
+++ shift-2d-grid.py
@@ -1,10 +1,17 @@
-class Solution:
-    def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
-        row, col = len(grid), len(grid[0])
-        res = [[0] * col for _ in range(row)]
-        for i in range(row):
-            for j in range(col):
-                new_col = (k + j) % col
-                new_row = (((k + j) // col) + i) % row
-                res[new_row][new_col] = grid[i][j]
-        return res
+1class Solution:
+2    def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+3        numbers = []
+4        row, col = len(grid), len(grid[0])
+5        for i in range(row):
+6            for j in range(col):
+7                numbers.append(grid[i][j])
+8        numbers = deque(numbers)
+9        for _ in range(k):
+10            numbers.appendleft(numbers.pop())
+11        res = [[0] * col for _ in range(row)]
+12        count = 0
+13        for i in range(row):
+14            for j in range(col):
+15                res[i][j] = numbers[count]
+16                count += 1
+17        return res
