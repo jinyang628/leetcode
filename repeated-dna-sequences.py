6a3054cd930bd6b53d29aec817a795737c969fb4
Jin Yang <chenjinyang4192@gmail.com>
2026-03-12 20:39:18 +0800

    Another day another leetcode submission

--- repeated-dna-sequences.py
+++ repeated-dna-sequences.py
@@ -1,10 +1,16 @@
-class Solution:
-    def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        st=set()
-        d={}
-        for i in range(len(s)-9):
-            if s[i:i+10] in d:
-                st.add(s[i:i+10])
-            else:
-                d[s[i:i+10]]=1
-        return list(st)
+1class Solution:
+2    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+3        counter = Counter()
+4        left = right = 0
+5        while right < len(s):
+6            if right < 9:
+7                right += 1
+8                continue
+9            counter[s[left: right + 1]] += 1
+10            left += 1
+11            right += 1
+12        res = []
+13        for key, freq in counter.items():
+14            if freq > 1:
+15                res.append(key)
+16        return res
