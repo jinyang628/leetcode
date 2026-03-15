3078877bb2ac2995b6dc67e476a84799ce047b3f
Jin Yang <chenjinyang4192@gmail.com>
2026-03-15 18:04:07 +0800

    Another day another leetcode submission

--- random-pick-with-weight.py
+++ random-pick-with-weight.py
@@ -1,23 +1,21 @@
-import random
-class Solution:
-    def __init__(self, w: List[int]):
-        total = 0
-        prefixSums = []
-        for num in w:
-            total += num
-            prefixSums.append(total)
-        self.prefixSums = prefixSums
-        self.total = total
-    def pickIndex(self) -> int:
-        left, right = 0, len(self.prefixSums) - 1
-        target = random.randint(1, self.total)
-        while left <= right:
-            mid = left + (right - left) // 2
-            if self.prefixSums[mid] >= target:
-                right = mid - 1
-            else:
-                left = mid + 1
-        return left
-# Your Solution object will be instantiated and called as such:
-# obj = Solution(w)
-# param_1 = obj.pickIndex()
+1class Solution:
+23    def __init__(self, w: List[int]):
+4        self.weights = []
+5        self.total = sum(w)
+6        curr = 0
+7        for num in w:
+8            curr += num
+9            self.weights.append(curr)
+1011    def pickIndex(self) -> int:
+12        num = random.uniform(0, self.total)
+13        left, right = 0, len(self.weights) - 1
+14        while left < right:
+15            mid = left + (right - left) // 2
+16            if self.weights[mid] < num:
+17                left = mid + 1
+18            else:
+19                right = mid
+20        return left
+21222324# Your Solution object will be instantiated and called as such:
+25# obj = Solution(w)
+26# param_1 = obj.pickIndex()
