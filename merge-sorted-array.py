b9d2a458ce73811ac72979bafd2650600004ea9d
Jin Yang <chenjinyang4192@gmail.com>
2026-03-17 20:14:55 +0800

    Another day another leetcode submission

--- merge-sorted-array.py
+++ merge-sorted-array.py
@@ -1,24 +1,24 @@
-class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        """
-        nums1_pointer = len(nums1) - 1
-        m -= 1
-        n -= 1
-        while m >= 0 and n >= 0:
-            if nums1[m] >= nums2[n]:
-                nums1[nums1_pointer] = nums1[m]
-                m -= 1
-            else:
-                nums1[nums1_pointer] = nums2[n]
-                n -= 1
-            nums1_pointer -= 1
-        while m >= 0:
-            nums1[nums1_pointer] = nums1[m]
-            nums1_pointer -= 1
-            m -= 1
-        while n >= 0:
-            nums1[nums1_pointer] = nums2[n]
-            nums1_pointer -= 1
-            n -= 1
-        return nums1
+1class Solution:
+2    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+3        """
+45        """
+6        idx = len(nums2)
+7        count = len(nums1) - 1
+8        while count - idx >= 0:
+9            nums1[count], nums1[count - idx] = nums1[count - idx], nums1[count]
+10            count -= 1
+1112        nums1_idx, nums2_idx = idx, 0
+13        res_idx = 0
+14        while nums1_idx < len(nums1) and nums2_idx < len(nums2):
+15            if nums1[nums1_idx] <= nums2[nums2_idx]:
+16                nums1[res_idx] = nums1[nums1_idx]
+17                nums1_idx += 1
+18            else:
+19                nums1[res_idx] = nums2[nums2_idx]
+20                nums2_idx += 1
+21            res_idx += 1
+22        while nums2_idx < len(nums2):
+23            nums1[res_idx] = nums2[nums2_idx]
+24            res_idx += 1
+25            nums2_idx += 1
+26
