a0a8b76c9d2c7f9997e167d47f48ef8fe79f6f8a
Jin Yang <chenjinyang4192@gmail.com>
2026-03-26 20:54:09 +0800

    Another day another leetcode submission

--- lucky-numbers-in-a-matrix.py
+++ lucky-numbers-in-a-matrix.py
@@ -1,15 +1,1 @@
-class Solution:
-    def luckyNumbers(self, matrix: List[List[int]]) -> List[int]:
-        row, col = len(matrix), len(matrix[0])
-        mins = [float('inf')] * row
-        maxs = [0] * col
-        for i in range(row):
-            for j in range(col):
-                mins[i] = min(mins[i], matrix[i][j])
-                maxs[j] = max(maxs[j], matrix[i][j])
-        total = []
-        track = set(maxs)
-        for num in mins:
-            if num in track:
-                total.append(num)
-        return total
+[[3,7,8],[9,11,13],[15,16,17]]
