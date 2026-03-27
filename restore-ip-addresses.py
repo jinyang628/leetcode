bd187d6042e59a2a860cb54413c2ba9677dd0cc1
Jin Yang <chenjinyang4192@gmail.com>
2026-03-27 14:31:41 +0800

    Another day another leetcode submission

--- restore-ip-addresses.py
+++ restore-ip-addresses.py
@@ -1,20 +1,22 @@
-class Solution:
-    def restoreIpAddresses(self, s: str) -> List[str]:
-        res = []
-        def backtrack(start: int, path: list[str]) -> None:
-            if len(path) == 4:
-                if start == len(s):
-                    res.append(".".join(path))
-                return
-            for length in range(1, 4):
-                if (start + length) > len(s):
-                    return 
-                segment = s[start: start + length]
-                if segment[0] == "0" and len(segment) > 1:
-                    return 
-                if 0 <= int(segment) <= 255:
-                    path.append(str(segment))
-                    backtrack(start+length, path)
-                    path.pop()
-        backtrack(0, [])
-        return res
+1class Solution:
+2    def restoreIpAddresses(self, s: str) -> List[str]:
+3        res = []
+4        def backtrack(idx: int, curr: str, subset: list[str]) -> None:
+5            print(idx, curr, subset)
+6            if len(subset) == 4:
+7                if idx == len(s):
+8                    res.append(".".join(subset))
+9                return
+1011            while idx < len(s):
+12                curr += s[idx]
+13                if len(curr) > 1 and curr[0] == "0":
+14                    return
+15                if int(curr) > 255:
+16                    return
+17                idx += 1
+18                subset.append(curr)
+19                backtrack(idx, "", subset)
+20                subset.pop()
+212223        backtrack(0, "", [])
+24        return res
+2526
