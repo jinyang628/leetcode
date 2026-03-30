59ec384414dfd71467fe02483b03bd8253b1c0a7
Jin Yang <chenjinyang4192@gmail.com>
2026-03-30 14:47:59 +0800

    Another day another leetcode submission

--- longest-turbulent-subarray.py
+++ longest-turbulent-subarray.py
@@ -1,19 +1,20 @@
-class Solution:
-    def maxTurbulenceSize(self, arr: List[int]) -> int:
-        maxSoFar = right = 1
-        left = 0
-        isRising = None
-        while right < len(arr):
-            print(left, right, isRising, maxSoFar)
-            if arr[right] > arr[right - 1] and (isRising or isRising is None):
-                isRising = False
-            elif arr[right] < arr[right - 1] and (not isRising):
-                isRising = True
-            else:
-                maxSoFar = max(maxSoFar, (right - left))
-                if arr[right] == arr[right - 1]:
-                    left = right
-                else:
-                    left = right - 1
-            right += 1
-        return max(maxSoFar, right - left)
+class Solution {
+    public int maxTurbulenceSize(int[] A) {
+        if (A == null) return 0;
+        if (A.length == 1) return 1;
+        boolean inc = A[0] < A[1];
+        int count = (A[0] != A[1]) ? 2 : 1;
+        int max = count;
+        for (int j = 2; j < A.length; ++j) {
+            if (inc && A[j-1] > A[j] || !inc && A[j-1] < A[j]) {
+                count++;
+            }
+            else {
+                count = (A[j-1] != A[j]) ? 2 : 1;
+            }
+            inc = A[j-1] < A[j];
+            max = Math.max(max, count);
+        } 
+        return max;
+    }
+}
