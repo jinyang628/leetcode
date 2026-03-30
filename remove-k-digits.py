67f17db4e5b9ff0f523cdf452d2e97e6625bb221
Jin Yang <chenjinyang4192@gmail.com>
2026-03-30 14:58:48 +0800

    Another day another leetcode submission

--- remove-k-digits.py
+++ remove-k-digits.py
@@ -1,15 +1,16 @@
-from collections import deque
-class Solution:
-    def removeKdigits(self, num: str, k: int) -> str:
-        stack = deque([])
-        for char in num:
-            while k and stack and int(stack[-1]) > int(char):
-                k -= 1
-                stack.pop()
-            stack.append(char)
-        while stack and stack[0] == "0":
-            stack.popleft()
-        while k and stack:
-            stack.pop()
-            k -= 1
-        return "".join(stack) if stack else "0"
+"1562219"
+3
+"30600"
+1
+"10"
+2
+"52374333400072837283091820"
+6
+"3238900989831"
+5
+"3238900989831"
+6
+"500"
+3
+"1234567"
+1
