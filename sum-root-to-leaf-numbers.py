e0799a20eb12c80ed84a9ff2b47b7ce53a8aa5c0
Jin Yang <chenjinyang4192@gmail.com>
2026-03-30 19:51:03 +0800

    Another day another leetcode submission

--- sum-root-to-leaf-numbers.py
+++ sum-root-to-leaf-numbers.py
@@ -1,25 +1,23 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        res = []
-        def helper(node: Optional[TreeNode], numbersSoFar: list[str]):
-            if not node:
-                return
-            numbersSoFar.append(str(node.val))
-            if not node.left and not node.right:
-                res.append(
-                    int("".join(numbersSoFar))
-                )
-                numbersSoFar.pop()
-                return
-            helper(node.left, numbersSoFar)
-            helper(node.right, numbersSoFar)
-            numbersSoFar.pop()
-        helper(root, [])
-        print(res)
-        return sum(res)
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+9        def dfs(node: Optional[TreeNode], path: list[str]) -> int:
+10            if not node:
+11                path.append(None)
+12                return 0
+13            path.append(str(node.val))
+14            if not node.left and not node.right:
+15                return int("".join(path))
+16            left = dfs(node.left, path) 
+17            path.pop()
+18            right = dfs(node.right, path)
+19            path.pop()
+20            print(left, right)
+21            return left + right
+22        return dfs(root, [])
+23
