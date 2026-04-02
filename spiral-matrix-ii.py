d7a1c9db35ac47ede42474c752beef3528973f6a
Jin Yang <chenjinyang4192@gmail.com>
2026-04-02 20:16:56 +0800

    Another day another leetcode submission

--- spiral-matrix-ii.py
+++ spiral-matrix-ii.py
@@ -1,37 +1,19 @@
-class Solution:
-    def generateMatrix(self, n: int) -> List[List[int]]:
-        mat = [[0] * n for _ in range(n)]
-        left = top = 0
-        right = bottom = n - 1
-        count = 1
-        while left <= right and top <= bottom:
-            original_left = left
-            while left <= right:
-                mat[top][left] = count
-                count += 1
-                left += 1
-            left = original_left
-            top += 1
-            original_top = top
-            while top <= bottom:
-                mat[top][right] = count
-                count += 1
-                top += 1
-            top = original_top
-            right -= 1
-            original_right = right
-            while right >= left:
-                mat[bottom][right] = count 
-                count += 1
-                right -= 1
-            right = original_right
-            bottom -= 1
-            original_bottom = bottom
-            while bottom >= top:
-                mat[bottom][left] = count
-                count += 1
-                bottom -= 1
-            bottom = original_bottom
-            left += 1
-        return mat
-        return mat
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int ans[][] = new int[n][n];
+        fillMatrix(ans, 0 , 0, 1,'r');
+        return ans;
+    }
+    public void fillMatrix(int [][]ans, int i, int j, int cur, char dir){
+        if(i < 0 || j < 0 || i >=ans.length ||  j >= ans.length) return;
+        if(ans[i][j] != 0) return;
+        ans[i][j] = cur;
+        if(dir == 'u'){
+            fillMatrix(ans, i-1,j,cur+1, 'u');    
+        }
+        fillMatrix(ans, i,j+1,cur+1, 'r');
+        fillMatrix(ans, i+1,j,cur+1, 'd');
+        fillMatrix(ans, i,j-1,cur+1, 'l');
+        fillMatrix(ans, i-1,j,cur+1, 'u');
+    }
+}
