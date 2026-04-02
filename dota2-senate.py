36fc1f492c54329700349ce0632ddf8ccd7a9633
Jin Yang <chenjinyang4192@gmail.com>
2026-04-02 20:01:46 +0800

    Another day another leetcode submission

--- dota2-senate.py
+++ dota2-senate.py
@@ -1,19 +1,28 @@
-from collections import deque
-class Solution:
-    def predictPartyVictory(self, senate: str) -> str:
-        r_queue = deque([])
-        d_queue = deque([])
-        for i in range(len(senate)):
-            if senate[i] == "R":
-                r_queue.append(i)
-            else:
-                d_queue.append(i)
-        counter = len(senate)
-        while r_queue and d_queue:
-            first_r, first_d = r_queue.popleft(), d_queue.popleft()
-            if first_r < first_d:
-                r_queue.append(first_r + counter)
-            else:
-                d_queue.append(first_d + counter)
-            counter += 1
-        return "Radiant" if not d_queue else "Dire"
+1from collections import deque, Counter
+2class Solution:
+3    def predictPartyVictory(self, senate: str) -> str:
+4        counter = Counter(senate)
+5        queue = deque(list(senate))
+6        dire_banned = radiant_banned = 0
+7        while queue:
+8            curr = queue.popleft()
+9            if curr == "R":
+10                if radiant_banned:
+11                    radiant_banned -= 1
+12                    counter["R"] -= 1
+13                else:
+14                    if not counter["D"]:
+15                        return "Radiant"
+16                    else:
+17                        dire_banned += 1
+18                        queue.append("R")
+19            else:
+20                if dire_banned:
+21                    dire_banned -= 1
+22                    counter["D"] -= 1
+23                else:
+24                    if not counter["R"]:
+25                        return "Dire"
+26                    else:
+27                        radiant_banned += 1
+28                        queue.append("D")
