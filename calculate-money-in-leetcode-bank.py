ef9448f8a566a4fdddc1ab2656731c2830d3d18e
Jin Yang <chenjinyang4192@gmail.com>
2026-04-02 12:57:31 +0800

    Another day another leetcode submission

--- calculate-money-in-leetcode-bank.py
+++ calculate-money-in-leetcode-bank.py
@@ -1,11 +1,8 @@
-class Solution:
-    def totalMoney(self, n: int) -> int:
-        fullWeeks = n // 7
-        spillOver = n % 7
-        total = 0
-        if fullWeeks:
-            first = 1 + 2 + 3 + 4 + 5 + 6 + 7
-            last = (7/2) * (fullWeeks + (fullWeeks + 6))
-            total += (fullWeeks/2) * (first + last)
-        remainder = (spillOver/2) * ((fullWeeks + 1) + (fullWeeks + 1) + (spillOver - 1))
-        return int(total + remainder)
+1class Solution:
+2    def totalMoney(self, n: int) -> int:
+3        full_sets = n // 7
+4        remainder = n - (full_sets * 7)
+5        remainder_sum = int((full_sets + 1 + full_sets + remainder) * (remainder / 2))
+6        full_set_sum = int((28 + 28 + (7 * (full_sets - 1))) * (full_sets / 2))
+7        return remainder_sum + full_set_sum
+8
