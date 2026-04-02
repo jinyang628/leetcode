ed7bf61582719a779279bd2c6fc56b4e0772a46f
Jin Yang <chenjinyang4192@gmail.com>
2026-04-02 19:35:25 +0800

    Another day another leetcode submission

--- maximum-sum-circular-subarray.py
+++ maximum-sum-circular-subarray.py
@@ -1,12 +1,29 @@
-class Solution:
-    def maxSubarraySumCircular(self, nums: List[int]) -> int:
-        maxSum = minSum = nums[0]
-        currMax = currMin = total = 0
-        prefixSums = []
-        for num in nums:
-            total += num
-            currMax = max(num, currMax + num)
-            maxSum = max(currMax, maxSum)
-            currMin = min(num, currMin + num)
-            minSum = min(currMin, minSum)
-        return max(maxSum, total - minSum) if maxSum > 0 else maxSum
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int N = nums.size(), sum = 0, ans = INT_MIN;
+        for(int i=0; i<N; i++){
+            nums.push_back(nums[i]);
+        }
+        int j = 0;
+        for(int i=0; i<2*N; i++){
+            sum += nums[i];
+            ans = max(ans, nums[i]);
+            if(sum <= 0){
+                sum = 0;
+                j = i+1;
+                continue;
+            }
+            int len = i-j+1;
+            if(len>N){
+                sum -= nums[j];
+                j++;
+            }
+            while(j <= i && nums[j] <= 0){
+                sum -= nums[j++];
+            }
+            if(sum) ans = max(ans, sum);
+        }
+        return ans;
+    }
+};
