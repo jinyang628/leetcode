65ecb2bb6cbbbdbea8e741dd810617f24c9ed575
Jin Yang <chenjinyang4192@gmail.com>
2026-04-06 11:49:36 +0800

    Another day another leetcode submission

--- find-k-closest-elements.py
+++ find-k-closest-elements.py
@@ -1,12 +1,12 @@
-class Solution:
-    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        left = 0
-        right = len(arr)
-        while right - left > k:
-            left_diff = abs(x - arr[left])
-            right_diff = abs(x - arr[right - 1])
-            if left_diff <= right_diff:
-                right -= 1
-            else:
-                left += 1
-        return arr[left: right]
+1class Solution:
+2    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+3        heap = []
+4        for idx, num in enumerate(arr):
+5            heap.append((abs(num - x), num))
+6        heapq.heapify(heap)
+7        res = []
+8        for _ in range(k):
+9            _, num = heapq.heappop(heap)
+10            res.append(num)
+11        res.sort()
+12        return res
