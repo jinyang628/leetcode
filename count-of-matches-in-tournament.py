add45042e3732a53cda48217a177521bbd5c1798
Jin Yang <chenjinyang4192@gmail.com>
2026-04-09 12:30:23 +0800

    Another day another leetcode submission

--- count-of-matches-in-tournament.py
+++ count-of-matches-in-tournament.py
@@ -1,13 +1,7 @@
 class Solution:
     def numberOfMatches(self, n: int) -> int:
-        res = 0
+        ans = 0
         while n > 1:
-            if n % 2 != 0:
-                n -= 1
-                res += n // 2
-                n //= 2 
-                n += 1
-            else:
-                res += n // 2
-                n //= 2 
-        return res
+            ans += n // 2
+            n = n // 2 + n % 2      
+        return ans
