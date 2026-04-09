01785049acd191c55cf167e74a1443b8cf392928
Jin Yang <chenjinyang4192@gmail.com>
2026-04-09 12:36:06 +0800

    Another day another leetcode submission

--- largest-local-values-in-a-matrix.py
+++ largest-local-values-in-a-matrix.py
@@ -1,10 +1,11 @@
 class Solution:
     def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
-        length = len(grid)
-        res = [[0] * (length - 2) for _ in range(length - 2)]
-        for i in range(length - 2):
-            for j in range(length - 2):
-                for r in range(i, i + 3):
-                    for c in range(j, j + 3):
-                        res[i][j] = max(res[i][j], grid[r][c])
-        return res
+        ans=[]
+        for i in range(1,len(grid)-1):
+            tmp=[]
+            for j in range(1,len(grid[0])-1):
+                tmp.append(max(grid[i-1][j-1],grid[i-1][j],grid[i-1][j+1]
+                              ,grid[i][j-1],grid[i][j],grid[i][j+1]
+                              ,grid[i+1][j-1],grid[i+1][j],grid[i+1][j+1]))
+            ans.append(tmp)
+        return ans
