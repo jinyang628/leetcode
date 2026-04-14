b466f09775510b6ce64598a744ac7e7506db2452
Jin Yang <chenjinyang4192@gmail.com>
2026-04-14 10:46:02 +0800

    Another day another leetcode submission

--- minimum-changes-to-make-alternating-binary-string.py
+++ minimum-changes-to-make-alternating-binary-string.py
@@ -1,22 +1,4 @@
 class Solution:
     def minOperations(self, s: str) -> int:
-        first = second = 0
-        for i in range(len(s)):
-            curr = s[i]
-            if i % 2:
-                if curr == "0":
-                    continue
-            else:
-                if curr == "1":
-                    continue
-            first += 1
-        for i in range(len(s)):
-            curr = s[i]
-            if i % 2:
-                if curr == "1":
-                    continue
-            else:
-                if curr == "0":
-                    continue
-            second += 1
-        return min(first, second)
+        n = s[::2].count('0') + s[1::2].count('1')
+        return min(n, len(s) - n)
