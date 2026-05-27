dee78c4d6eaaa15f63d5ff092fbd9c55ba8a12a8
Jin Yang <chenjinyang4192@gmail.com>
2026-05-27 13:29:46 +0800

    Another day another leetcode submission

--- 4sum.py
+++ 4sum.py
@@ -1,28 +1,32 @@
-class Solution:
-    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        nums.sort()
-        res = []
-        for i in range(len(nums) - 3):
-            # skip duplicate starting values
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            for j in range(i+1, len(nums) - 2):
-                # skip duplicate starting values
-                if j > i + 1 and nums[j] == nums[j-1]:
-                    continue
-                left, right = j + 1, len(nums) - 1
-                while left < right:
-                    four_sum = nums[i] + nums[j] + nums[left] + nums[right]
-                    if four_sum == target:
-                        res.append([nums[i], nums[j], nums[left], nums[right]])
-                        left += 1
-                        right -= 1
-                        while left < right and nums[left] == nums[left - 1]:
-                            left += 1
-                        while left < right and nums[right] == nums[right + 1]:
-                            right -= 1
-                    elif four_sum < target:
-                        left += 1
-                    else:
-                        right -= 1
-        return res
+1class Solution:
+2    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+3        nums.sort()
+4        res = []
+5        first = 0
+6        while first < len(nums) - 3:
+7            second = first + 1
+8            while second < len(nums) - 2:
+9                third = second + 1
+10                fourth = len(nums) - 1
+11                while third < fourth:
+12                    curr = nums[first] + nums[second] + nums[third] + nums[fourth]
+13                    if curr == target:
+14                        res.append([nums[first], nums[second], nums[third], nums[fourth]])
+15                        third += 1
+16                        while third < fourth and nums[third] == nums[third - 1]:
+17                            third += 1
+18                    elif curr > target:
+19                        fourth -= 1
+20                        while third < fourth and nums[fourth] == nums[fourth + 1]:
+21                            fourth -= 1
+22                    else:
+23                        third += 1
+24                        while third < fourth and nums[third] == nums[third - 1]:
+25                            third += 1
+2627                second += 1
+28                while second < len(nums) - 2 and nums[second] == nums[second - 1]:
+29                    second += 1
+30            first += 1
+31            while first < len(nums) - 3 and nums[first] == nums[first - 1]:
+32                first += 1
+33        return res
