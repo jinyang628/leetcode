2bd492638c318a1483bd750e4370426bd8993510
Jin Yang <chenjinyang4192@gmail.com>
2026-05-28 13:48:20 +0800

    Another day another leetcode submission

--- reverse-substrings-between-each-pair-of-parentheses.py
+++ reverse-substrings-between-each-pair-of-parentheses.py
@@ -1,14 +1,4 @@
-class Solution:
-    def reverseParentheses(self, s: str) -> str:
-        stack = []
-        for char in s:
-            if char == ")":
-                substring = []
-                while stack[-1] != "(":
-                    substring.append(stack.pop()[::-1])
-                    pass
-                stack.pop()
-                stack.append("".join(substring))
-            else:
-                stack.append(char)
-        return "".join(stack)
+forward ->  ( ... ( ... ) ... )  <- backward
+             ^     ^    ^     ^
+             |     |    |     |
+             A-----B----b-----a
