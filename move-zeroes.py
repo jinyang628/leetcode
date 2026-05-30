8501c071e60d5e7298682818cca5fe4d3cca180f
Jin Yang <chenjinyang4192@gmail.com>
2026-05-30 16:44:45 +0800

    Another day another leetcode submission

---
+++ move-zeroes.py
@@ -0,0 +1,10 @@
+class Solution:
+    def moveZeroes(self, nums):
+        """
+        """
+        pos = 0
+        for i in range(len(nums)):
+            el = nums[i]
+            if el != 0:
+                nums[pos], nums[i] = nums[i], nums[pos]
+                pos += 1
