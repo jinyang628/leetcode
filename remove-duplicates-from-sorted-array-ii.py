098e2ae96a18ff332227f762ad806c6209c3bd0c
Jin Yang <chenjinyang4192@gmail.com>
2026-05-30 17:10:26 +0800

    Another day another leetcode submission

--- remove-duplicates-from-sorted-array-ii.py
+++ remove-duplicates-from-sorted-array-ii.py
@@ -1,1 +1,12 @@
-Input: nums = [1,1,1,2,2,3]
+1class Solution:
+2    def removeDuplicates(self, nums: List[int]) -> int:
+3        left = right = 0
+4        while right < len(nums):
+5            if left < 2 or nums[right] != nums[left - 2]:
+6                nums[left], nums[right] = nums[right], nums[left]
+7                left += 1
+8                right += 1
+9            else:
+10                right += 1
+11        return left
+12
