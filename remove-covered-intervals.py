750be600498c11595abc1b70aa54de72d1e8166d
Jin Yang <chenjinyang4192@gmail.com>
2026-07-18 11:21:27 +0800

    Another day another leetcode submission

--- remove-covered-intervals.py
+++ remove-covered-intervals.py
@@ -1,12 +1,9 @@
 1class Solution:
 2    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
-3        intervals.sort(key=lambda x: (x[0], -x[1]))
-4        removed_intervals = set()
-5        for i in range(len(intervals) - 1):
-6            if i in removed_intervals:
-7                continue
-8            start, end = intervals[i]
-9            for j in range(i + 1, len(intervals)):
-10                if start <= intervals[j][0] and intervals[j][1] <= end:
-11                    removed_intervals.add(j)
-1213        return len(intervals) - len(removed_intervals)
+3        intervals.sort(key=lambda x: (x[1], -x[0]))
+4        res = [intervals[0]]
+5        for i in range(1, len(intervals)):
+6            while res and intervals[i][0] <= res[-1][0]:
+7                res.pop()
+8            res.append(intervals[i])
+9        return len(res)
