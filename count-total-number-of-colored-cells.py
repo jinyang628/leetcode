1082f6e541ecbe8d4e090d17e798338338d0d4e4
Jin Yang <chenjinyang4192@gmail.com>
2026-07-18 11:26:12 +0800

    Another day another leetcode submission

--- count-total-number-of-colored-cells.py
+++ count-total-number-of-colored-cells.py
@@ -1,9 +1,3 @@
-1class Solution:
-2    def coloredCells(self, n: int) -> int:
-3        if n == 1:
-4            return 1
-5        # 4 8 12
-6        # increments incremented by 4
-7        n -= 1
-8        total_increments = int((n / 2) * (4 + 4 + (4 * (n - 1))))
-9        return total_increments + 1
+class Solution:
+    def coloredCells(self, n: int) -> int:
+        return n**2 + (n - 1)**2
