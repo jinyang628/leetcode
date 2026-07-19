1a008c3d98d8aca9f067be588d9a8c082c66d527
Jin Yang <chenjinyang4192@gmail.com>
2026-07-19 12:11:11 +0800

    Another day another leetcode submission

--- decode-string.py
+++ decode-string.py
@@ -1,16 +1,20 @@
 1class Solution:
 2    def decodeString(self, s: str) -> str:
-3        stack = []
-4        for i in range(len(s)):
-5            if s[i] != "]":
-6                stack.append(s[i])
-7            else:
-8                substr = ""
-9                while stack and stack[-1] != "[":
-10                    substr = stack.pop() + substr
-11                stack.pop()
-12                multiplier = ""
-13                while stack and stack[-1].isdigit():
-14                    multiplier = stack.pop() + multiplier
-15                stack.append(int(multiplier) * substr)
-16        return "".join(stack)
+3        def construct(index: int) -> tuple[str, int]:
+4            res = ""
+5            multiplier = ""
+6            while index < len(s) and s[index] != "]":
+7                if s[index].isdigit():
+8                    multiplier += s[index]
+9                elif s[index] == "[":
+10                    index += 1
+11                    substring, index = construct(index)
+12                    res += int(multiplier) * substring
+13                    multiplier = ""
+14                else:
+15                    res += s[index]
+16                index += 1
+17            return res, index
+18        res, _ = construct(0)
+19        return res
+20
