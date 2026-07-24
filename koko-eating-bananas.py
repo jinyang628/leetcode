4799df2393e2f0cc9660f94353f157ed30b36169
Jin Yang <chenjinyang4192@gmail.com>
2026-07-24 12:58:09 +0800

    Another day another leetcode submission

--- koko-eating-bananas.py
+++ koko-eating-bananas.py
@@ -1,19 +1,17 @@
 1class Solution:
 2    def minEatingSpeed(self, piles: List[int], h: int) -> int:
-3        minimum, maximum  = 1, max(piles)
-45        def canFinish(speed: int, idx: int, timeLeft: int) -> bool:
-6            if idx == len(piles) and timeLeft >= 0:
-7                return True
-8            if timeLeft <= 0:
-9                return False
-10            timeNeeded = math.ceil(piles[idx] / speed)
-11            return canFinish(speed, idx + 1, timeLeft - timeNeeded)
-1213        lastValidSpeed = None
-14        while minimum <= maximum:
-15            speed = minimum + (maximum - minimum) // 2
-16            if canFinish(speed, 0, h):
-17                lastValidSpeed = speed
-18                maximum = speed - 1
-19            else:
-20                minimum = speed + 1
-2122        return lastValidSpeed
+3        left = 1
+4        right = max(piles)
+5        def canFinish(speed: int) -> bool:
+6            res = 0
+7            for pile in piles:
+8                res += math.ceil(pile / speed)
+9            return res <= h
+10        while left <= right:
+11            mid = left + (right - left) // 2
+12            if canFinish(mid):
+13                right = mid - 1
+14            else:
+15                left = mid + 1
+16        return left
+17
