9ed2655cd0f8bd7218c4bc02017ff29a0cf15e26
Jin Yang <chenjinyang4192@gmail.com>
2026-07-31 11:25:29 +0800

    Another day another leetcode submission

--- maximum-number-of-removable-characters.py
+++ maximum-number-of-removable-characters.py
@@ -1,22 +1,24 @@
-class Solution:
-    def maximumRemovals(self, s: str, p: str, removable: List[int]) -> int:
-        left, right = 0, len(removable) - 1
-        def isSubseq(removeEndIdx: int) -> bool:
-            removed_idxes = set(removable[:removeEndIdx])
-            s_pointer = p_pointer = 0
-            while s_pointer < len(s) and p_pointer < len(p):
-                if s_pointer in removed_idxes:
-                    s_pointer += 1
-                elif s[s_pointer] == p[p_pointer]:
-                    s_pointer += 1
-                    p_pointer += 1
-                else:
-                    s_pointer += 1
-            return p_pointer == len(p)
-        while left <= right:
-            mid = left + (right - left) // 2
-            if isSubseq(mid + 1):
-                left = mid + 1
-            else:
-                right = mid - 1
-        return left
+"abcdefg"
+"bdg"
+[1, 2, 4, 5, 3]
+"abcdefghijklmnoabcdefghijklmno"
+"acegikmoacegikmo"
+[1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27]
+"zzzzzyyyyyxxxxxwwwwwvvvvvuuuuuw"
+"zvuw"
+[1, 6, 11, 16, 21, 26]
+"aaaaaabbbbbbccccccddddddeeeeee"
+"abcde"
+[0, 6, 12, 18, 24]
+"pqrstuvwxyzabcdefghijklm"
+"pqrt"
+[1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
+"aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbcccccccccccccccccc"
+"aabc"
+[15, 16, 17, 18, 19, 30, 31, 32, 33, 34, 45, 46, 47, 48, 49]
+"zzxxccvvbbnnaaammmllljjjkkk"
+"zvbmjk"
+ [1, 2, 4, 5, 7, 8, 10, 11, 13, 14, 16, 17, 19, 20, 22, 23]
+ "abcdefghijklmnopqrstuvwxyabcdefghij"
+"acegi"
+[1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
