7570595236ca6d8c23a00fdc50b148ce12975aba
Jin Yang <chenjinyang4192@gmail.com>
2026-07-31 10:31:38 +0800

    Another day another leetcode submission

--- maximize-ysum-by-picking-a-triplet-of-distinct-xvalues.py
+++ maximize-ysum-by-picking-a-triplet-of-distinct-xvalues.py
@@ -1,15 +1,19 @@
 1class Solution:
 2    def maxSumDistinctTriplet(self, x: List[int], y: List[int]) -> int:
-3        groupings = defaultdict(list)
+3        idx_tracker = defaultdict(list)
 4        for idx, num in enumerate(x):
-5            groupings[num].append(y[idx])
-6        largest_in_group = []
-7        for _, items in groupings.items():
-8            largest_in_group.append(-max(items))
-9        if len(largest_in_group) <= 2:
-10            return -1
-11        heapq.heapify(largest_in_group)
-12        res = 0
-13        for _ in range(3):
-14            res += -heapq.heappop(largest_in_group)
-15        return res
+5            idx_tracker[num].append(idx)
+67        candidates = []
+8        for _, indexes in idx_tracker.items():
+9            maxSoFar = 0
+10            for idx in indexes:
+11                maxSoFar = max(maxSoFar, y[idx])
+12            candidates.append(-maxSoFar)
+1314        heapq.heapify(candidates)
+15        res = 0
+16        if len(candidates) < 3:
+17            return -1
+18        for _ in range(3):
+19            res += -heapq.heappop(candidates)
+20        return res
+21
