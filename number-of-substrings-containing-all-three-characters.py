9e1a27d78b1d16e11d64c818d020657883786fa6
Jin Yang <chenjinyang4192@gmail.com>
2026-08-04 15:20:57 +0800

    Another day another leetcode submission

--- number-of-substrings-containing-all-three-characters.py
+++ number-of-substrings-containing-all-three-characters.py
@@ -1,8 +1,13 @@
-# | A U B U C | -  | A | - | B | - | C | + | B ∩ C | + | A ∩ C | + | A ∩ B |
-result =( n * (n + 1) ) // 2 #  | A U B U C | 
-result += include('a') # | A | 
-result += include('b') # | B | 
-result += include('c') # | C | 
-result -= exclude('a') #  | B |  + | C |  -  | B ∩ C | 
-result -= exclude('b') #  | A | + | C |  -  | A ∩ C | 
-result -= exclude('c') #  | A | + | B | - | A ∩ B |
+1class Solution:
+2    def numberOfSubstrings(self, s: str) -> int:
+3        res = 0
+4        left = right = 0
+5        counter = Counter()
+6        while right < len(s):
+7            counter[s[right]] += 1
+8            while counter["a"] > 0 and counter["b"] > 0 and counter["c"] > 0:
+9                res += len(s) - right
+10                counter[s[left]] -= 1
+11                left += 1
+12            right += 1
+13        return res
