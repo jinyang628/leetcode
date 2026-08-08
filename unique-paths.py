c689d3280ad62417c3c8a0ed10df6b7d814cbef3
Jin Yang <chenjinyang4192@gmail.com>
2026-08-08 09:54:04 +0800

    Another day another leetcode submission

--- unique-paths.py
+++ unique-paths.py
@@ -1,11 +1,8 @@
 1class Solution:
 2    def uniquePaths(self, m: int, n: int) -> int:
-3        res = [[0] * n for _ in range(m)]
-4        for i in range(n):
-5            res[0][i] = 1
-6        for i in range(m):
-7            res[i][0] = 1
-8        for i in range(1, m):
-9            for j in range(1, n):
-10                res[i][j] = res[i - 1][j] + res[i][j - 1]
-11        return res[-1][-1]
+3        dp = [[0] * (n + 1) for _ in range(m + 1)]
+4        dp[0][1] = 1
+5        for i in range(1, m + 1):
+6            for j in range(1, n + 1):
+7                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+8        return dp[-1][-1]
