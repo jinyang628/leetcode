273b016c4a3e5d79f84966bd13a640bcdd196668
Jin Yang <chenjinyang4192@gmail.com>
2026-08-12 17:56:00 +0800

    Another day another leetcode submission

--- continuous-subarray-sum.py
+++ continuous-subarray-sum.py
@@ -1,13 +1,12 @@
-public boolean checkSubarraySum(final int[] nums, final int k) {
-    var sumsSet = new HashSet<Integer>(nums.length);
-    int sum = 0, prevSum;
-    for (int i = 0; i < nums.length; i++) {
-        prevSum = sum;
-        sum = (sum + nums[i]) % k;
-        if (sumsSet.contains(sum)) {
-            return true;
-        }
-        sumsSet.add(prevSum);
-    }
-    return false;
-}
+1class Solution:
+2    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+3        remainders = {0: -1} # key is remainder, value is index
+4        curr = 0
+5        for idx, num in enumerate(nums):
+6            curr += num
+7            remainder = curr % k 
+8            if remainder in remainders and remainders[remainder] + 2 <= idx:
+9                return True
+10            if remainder not in remainders:
+11                remainders[remainder] = idx
+12        return False
